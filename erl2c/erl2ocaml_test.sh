cc4428c72853abb633c04ccf34688dfa7310c297
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-04-01 15:19:45 +0100

    erl2ocaml: simpler testing script

--- erl2c/erl2ocaml_test.sh
+++ erl2c/erl2ocaml_test.sh
@@ -1,35 +1,27 @@
+set -x
 set -e
 
 (cd ..; make;)
 
-# TODO - write a simple boilerplate function here
-./bin/erl2ocaml -erl erl2ocaml_examples/mod01.erl -ml erl2ocaml_examples/mod01.raw._.ml -mli erl2ocaml_examples/mod01.raw._.mli
-./bin/erl2ocaml -erl erl2ocaml_examples/mod02.erl -ml erl2ocaml_examples/mod02.raw._.ml -mli erl2ocaml_examples/mod02.raw._.mli
-./bin/erl2ocaml -erl erl2ocaml_examples/mod03.erl -ml erl2ocaml_examples/mod03.raw._.ml -mli erl2ocaml_examples/mod03.raw._.mli
-./bin/erl2ocaml -erl erl2ocaml_examples/mod04.erl -ml erl2ocaml_examples/mod04.raw._.ml -mli erl2ocaml_examples/mod04.raw._.mli
+for m in "mod01" "mod02" "mod03" "mod04"
+do
+    ./bin/erl2ocaml -erl "erl2ocaml_examples/${m}.erl" -ml "erl2ocaml_examples/${m}.raw._.ml" -mli "erl2ocaml_examples/${m}.raw._.mli"
+done
 
 (cd erl2ocaml_examples;
 
-ocamlformat --enable-outside-detected-project mod01.raw._.mli -o mod01._.mli;
-diff mod01._.mli mod01.mli;
-ocamlformat --enable-outside-detected-project mod01.raw._.ml -o mod01._.ml;
-diff mod01._.ml mod01.ml;
+rm -f *.cmi *.cmo
+ocamlc -c Ffi.ml;
 
-ocamlformat --enable-outside-detected-project mod02.raw._.mli -o mod02._.mli;
-diff mod02._.mli mod02.mli;
-ocamlformat --enable-outside-detected-project mod02.raw._.ml -o mod02._.ml;
-diff mod02._.ml mod02.ml;
-
-ocamlformat --enable-outside-detected-project mod03.raw._.mli -o mod03._.mli;
-diff mod03._.mli mod03.mli;
-ocamlformat --enable-outside-detected-project mod03.raw._.ml -o mod03._.ml;
-diff mod03._.ml mod03.ml;
-
-ocamlformat --enable-outside-detected-project mod04.raw._.mli -o mod04._.mli;
-diff mod04._.mli mod04.mli;
-ocamlformat --enable-outside-detected-project mod04.raw._.ml -o mod04._.ml;
-diff mod04._.ml mod04.ml;
-
-ocamlc -c Ffi.ml mod01.mli mod01.ml mod02.mli mod02.ml mod03.mli mod03.ml mod04.mli mod04.ml;)
+for m in "mod01" "mod02" "mod03" "mod04"
+do
+    ocamlformat --enable-outside-detected-project "${m}.raw._.mli" -o "${m}._.mli";
+    diff "${m}._.mli" "${m}.mli";
+    ocamlformat --enable-outside-detected-project "${m}.raw._.ml" -o "${m}._.ml";
+    diff "${m}._.ml" "${m}.ml";
+    ocamlc -c "${m}.mli" "${m}.ml";
+done
+rm -f *.cmi *.cmo;
+)
 
 echo "OK"
