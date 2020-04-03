3822eb1cc7d39e2325dc344e44297d47b6effaa0
Richard Carlsson <carlsson.richard@gmail.com>
2020-04-03 12:39:46 +0200

    Fix example files

--- examples/src/mod04.erl
+++ examples/src/mod04.erl
@@ -4,29 +4,33 @@
 
 -depends_on([mod03]).
 
--spec mk_unit_remote() -> mod03:unit0().
-mk_unit_remote() -> mod03:unit0{}.
+% colon qualifier cannot be used for enums
+%-spec mk_unit_remote() -> mod03:unit0().
+%mk_unit_remote() -> mod03.unit0{}.
 
 -spec mk_unit_remote_dot() -> mod03:unit0().
 mk_unit_remote_dot() -> mod03.unit0{}.
 
--spec mk_left_remote(A) -> mod03:either(A, _).
-mk_left_remote(A) -> mod03:left{A}.
+% colon qualifier cannot be used for enums
+%-spec mk_left_remote(A) -> mod03:either(A, _).
+%mk_left_remote(A) -> mod03:left{A}.
 
 -spec mk_left_remote_dot(A) -> mod03:either(A, _).
 mk_left_remote_dot(A) -> mod03.left{A}.
 
--spec mk_right_remote(B) -> mod03:either(_, B).
-mk_right_remote(B) -> mod03:right{B}.
+% colon qualifier cannot be used for enums
+%-spec mk_right_remote(B) -> mod03:either(_, B).
+%mk_right_remote(B) -> mod03:right{B}.
 
 -spec mk_right_remote_dot(B) -> mod03:either(_, B).
 mk_right_remote_dot(B) -> mod03.right{B}.
 
--spec zero_remote(mod03:unit0(), V) -> V.
-zero_remote(U, Val) ->
-  case U of
-    mod03:unit0{} -> Val
-  end.
+% colon qualifier cannot be used for enums
+%-spec zero_remote(mod03:unit0(), V) -> V.
+%zero_remote(U, Val) ->
+%  case U of
+%    mod03:unit0{} -> Val
+%  end.
 
 -spec zero_remote_dot(mod03:unit0(), V) -> V.
 zero_remote_dot(U, Val) ->
@@ -42,11 +46,12 @@ end.
 -spec zero_remote1_dot(mod03:unit0(), V) -> V.
 zero_remote1_dot(mod03.unit0{}, Val) -> Val.
 
--spec un_pair_remote(mod03:pair(A,B)) -> {A,B}.
-un_pair_remote(Pair) ->
-  case Pair of
-    mod03:pair{A, B} -> {A, B}
-end.
+% colon qualifier cannot be used for enums
+%-spec un_pair_remote(mod03:pair(A,B)) -> {A,B}.
+%un_pair_remote(Pair) ->
+%  case Pair of
+%    mod03:pair{A, B} -> {A, B}
+%end.
 
 -spec un_pair_remote_dot(mod03:pair(A,B)) -> {A,B}.
 un_pair_remote_dot(Pair) ->
