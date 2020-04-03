a69b8f3896208c639f8178f58f6273785ebd562b
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-04-03 21:23:11 +0100

    erl2.mk - targets for exposing IR (internal representation)

--- erl2.mk
+++ erl2.mk
@@ -10,3 +10,32 @@ ERLC_DEPSCAN := $(ERLC_COMPILE) +makedep2
 
 
 include $(ROOT)/erlbuild/erlbuild.mk
+
+# Helper targets for developement and hacking
+
+IR_DIR = $(BUILD_DIR)/ir
+IR_SPEC_DIR = ../ir-spec
+
+MLS := $(sort $(wildcard $(BUILD_DIR)/ocaml/*.ml) $(wildcard $(BUILD_DIR)/ocaml/*.mli))
+
+ocamlformat:
+	rm -f $(IR_DIR)/*.mli
+	rm -f $(IR_DIR)/*.ml
+	mkdir -p $(IR_DIR)
+	$(foreach ml, $(MLS), ocamlformat --enable-outside-detected-project $(ml) -o $(IR_DIR)/$(notdir $(ml));)
+
+p:
+	rm -f $(IR_DIR)/*.P
+	mkdir -p $(IR_DIR)
+	$(foreach erl, $(SOURCES), $(ERLC_COMPILE) -P -o $(IR_DIR) $(erl);)
+
+ir: ocamlformat p
+
+test-ir: ir
+	diff -r $(BUILD_DIR)/ir ../ir-spec/
+
+update-ir-spec: ir
+	rm -rf $(IR_SPEC_DIR)
+	cp -r $(IR_DIR) $(IR_SPEC_DIR)
+
+.PHONY: ocamlformat p ir test-ir update-ir-spec
--- examples/src/README.md
+++ examples/src/README.md
@@ -5,6 +5,15 @@
     (make -j also works)
 
 
+# To peep into erl2 internals
+
+    make ir
+
+`make ir` puts intermediate representation (IR) of the build into `$(BUILD_DIR)/ir` folder. Namely:
+
+- `.ml` and `.mli` files - `ocamlformat`ted representation of erl2 files (with `-lang([erl2, st]).`)
+- `.P` files - erl1 representation of erl2 files.
+
 # How to debug builds
 
     make VERBOSE=2
