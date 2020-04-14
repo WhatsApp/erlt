b8d91f61068e9b795a5d6330110ec937f000b677
Richard Carlsson <carlsson.richard@gmail.com>
2020-04-14 14:14:16 +0200

    Update enum examples

--- examples/dev_enum/src/enum_mod02.erl
+++ examples/dev_enum/src/enum_mod02.erl
@@ -8,32 +8,32 @@
 -type possibly(T) :: enum_mod01.possibly(T).
 
 %% local type using remote constructors
--type perhaps(T) :: enum_mod01.some{T} | enum_mod01.none{}.
+-type perhaps(T) :: enum_mod01.possibly.some{T} | enum_mod01.possibly.none{}.
 
 
 -spec p() -> possibly(any()).
 
 p() ->
-    %% only dots can be used for constructor module qualifiers
-    enum_mod01.none{}.
+    %% only dots can be used for constructor module qualifiers, not colon
+    enum_mod01.possibly.none{}.
 
 
 -spec q(T) -> perhaps(T).
 
 q(X) ->
-    enum_mod01.some{X}.
+    enum_mod01.possibly.some{X}.
 
 
 -spec r(perhaps(T)) -> [T].
 
 %% remote constructors in patterns
-r(enum_mod02.some{X=41}) ->
+r(enum_mod02.possibly.some{X=41}) ->
     X+1;
 r(E) ->
     case E of
-        enum_mod02.none{} ->
+        enum_mod02.possibly.none{} ->
             [];
-        enum_mod02.some{X} ->
+        enum_mod02.possibly.some{X} ->
             [X]
     end.
 
@@ -42,11 +42,11 @@ r(E) ->
 s(E) ->
     %% remote constructors in try-patterns
     try E of
-        enum_mod02.none{} ->
+        enum_mod02.possibly.none{} ->
             [];
-        enum_mod02.some{X} ->
+        enum_mod02.possibly.some{X} ->
             [X]
     catch
-        throw: enum_mod02.some{thing} : Trace ->
+        throw: enum_mod02.possibly.some{thing} : Trace ->
             {caught, Trace}
     end.
