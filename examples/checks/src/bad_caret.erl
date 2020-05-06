1ed1746b2b533937ed7f0b76ad6bebb4636b0376
Richard Carlsson <carlsson.richard@gmail.com>
2020-05-06 10:57:38 +0200

    Disallow caret prefix op on underscore-patterns and in general exprs

---
+++ examples/checks/src/bad_caret.erl
@@ -0,0 +1,23 @@
+-lang([erl2, st]).
+-module(bad_caret).
+
+-export([f/1, g/1, h/1]).
+
+%% Error: caret-marked variables cannot be used in normal expressions
+f(X) ->
+    {ok, ^X}.
+
+%% Error: caret-marked variables cannot be used in guard expressions
+g(X) ->
+    case X of
+        {foo, Y} when Y =:= ^X -> ok;
+        _ -> error
+    end.
+
+%% Error: caret prefix can only be used on variables
+h(X) ->
+    case X of
+        {^foo, _} -> ok;
+        {foo, ^_} -> ok;
+        _ -> X + ^42
+    end.
