c79e18670561c5b2bdcf69e3743fdbba407fa99f
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-05-11 00:50:58 +0100

    stErlang: more utils

--- sterlang/src/main/scala/com/whatsapp/sterlang/SyntaxUtil.scala
+++ sterlang/src/main/scala/com/whatsapp/sterlang/SyntaxUtil.scala
@@ -17,6 +17,8 @@
 package com.whatsapp.sterlang
 
 object SyntaxUtil {
+  val S = Ast
+  val A = Absyn
 
   def collectPatVars(pat: S.Pat): List[String] =
     pat match {
@@ -43,6 +45,32 @@ object SyntaxUtil {
         pats.flatMap(collectPatVars)
       case S.ConsPat(hPat, tPat) =>
         collectPatVars(hPat) ++ collectPatVars(tPat)
+    }
+
+  def collectPatVars2(pat: A.Pat): List[String] =
+    pat match {
+      case A.WildPat =>
+        List.empty
+      case A.VarPat(v) =>
+        List(v)
+      case A.TuplePat(tPats) =>
+        tPats.map(_.pat1).flatMap(collectPatVars2)
+      case A.BoolPat(_) =>
+        List.empty
+      case A.NumberPat(_) =>
+        List.empty
+      case A.StringPat(_) =>
+        List.empty
+      case A.RecordPat(fields, _) =>
+        fields.map(_.value.pat1).flatMap(collectPatVars2)
+      case A.AndPat(p1, p2) =>
+        collectPatVars2(p1.pat1) ++ collectPatVars2(p2.pat1)
+      case A.EnumCtrPat(_, _, tPats) =>
+        tPats.map(_.pat1).flatMap(collectPatVars2)
+      case A.ListPat(tPats) =>
+        tPats.map(_.pat1).flatMap(collectPatVars2)
+      case A.ConsPat(hPat, tPat) =>
+        collectPatVars2(hPat.pat1) ++ collectPatVars2(tPat.pat1)
     }
 
   def collectNamedTypeVars(t: S.Type): List[String] =
@@ -198,4 +226,291 @@ object SyntaxUtil {
         scc :: properSort(rest, usage)
     }
 
+  def moduleApi(module: String, program: S.Program): ModuleApi = {
+    val names =
+      (program.enumDefs.map(_.name) ++ program.typeAliases.map(_.name) ++ program.opaques.map(_.name)).toSet
+    val enumDefs1 = program.enumDefs.collect {
+      case S.EnumDef(name, params, cons) if program.exportTypes((name, params.size)) =>
+        val name1 = module + ":" + name
+        val cons1 = cons.map {
+          case S.EnumCon(cName, tps) =>
+            S.EnumCon(cName, tps.map(globalizeType(module, names)))(Pos.NP)
+        }
+        S.EnumDef(name1, params, cons1)(Pos.NP)
+    }
+    val aliases1 = program.typeAliases.collect {
+      case S.TypeAlias(name, params, tp) if program.exportTypes((name, params.size)) =>
+        val name1 = module + ":" + name
+        val tp1 = globalizeType(module, names)(tp)
+        S.TypeAlias(name1, params, tp1)(Pos.NP)
+    }
+    val specs1 = program.specs.collect {
+      case S.Spec(n:S.LocalFunName, ft@S.FunType(argTypes, resType)) if program.exports((n.name, n.arity)) =>
+        val name1 = new S.RemoteFunName(module, n.name, n.arity)
+        val funType1 =
+          S.FunType(argTypes.map(globalizeType(module, names)), globalizeType(module, names)(resType))(ft.p)
+        S.Spec(name1, funType1)(Pos.NP)
+    }
+    val opaques = program.opaques.collect {
+      case S.Opaque(name, params, _) if program.exportTypes((name, params.size)) =>
+        S.TypeId(S.RemoteName(module, name), params.size)
+    }
+    ModuleApi(enumDefs1, aliases1, specs1, opaques)
+  }
+
+  private def globalizeType(module: String, names: Set[String])(tp: S.Type): S.Type = tp match {
+    case S.TypeVar(_) =>
+      tp
+    case S.WildTypeVar() =>
+      tp
+    case S.TupleType(params) =>
+      S.TupleType(params.map(globalizeType(module, names)))(tp.p)
+    case S.RecordType(fields) =>
+      val fields1 = fields.map{f => S.Field(f.label, globalizeType(module, names)(f.value))}
+      S.RecordType(fields1)(tp.p)
+    case S.OpenRecordType(fields, rt) =>
+      val fields1 = fields.map{f => S.Field(f.label, globalizeType(module, names)(f.value))}
+      S.OpenRecordType(fields1, rt)(tp.p)
+    case S.FunType(argTypes, resType) =>
+      S.FunType(argTypes.map(globalizeType(module, names)), globalizeType(module, names)(resType))(tp.p)
+    case S.ListType(elemType) =>
+      S.ListType(globalizeType(module, names)(elemType))(tp.p)
+    case S.UserType(name, params) =>
+      val name1 = name match {
+        case S.LocalName(name) =>
+          if (names(name))
+            S.RemoteName(module, name)
+          else
+            S.LocalName(name)
+        case _ => name
+      }
+      S.UserType(name1, params.map(globalizeType(module, names)))(tp.p)
+  }
+
+  def normalizeTypes(program: S.Program): S.Program = {
+    def normEnumCon(con: S.EnumCon): S.EnumCon =
+      con.copy(argTypes = con.argTypes.map(normalizeType(program)))(con.p)
+    val enumDefs1 =
+      program.enumDefs.map{ed => ed.copy(cons = ed.cons.map(normEnumCon))(ed.p)}
+    val typeAliases1 =
+      program.typeAliases.map{ta => ta.copy(body = normalizeType(program)(ta.body))(ta.p)}
+    val opaques1 =
+      program.opaques.map{o => o.copy(body = normalizeType(program)(o.body))(o.p)}
+    val specs1 =
+      program.specs.map(s => s.copy(funType = normFunType(program, s.funType))(s.p))
+    program.copy(
+      enumDefs = enumDefs1,
+      typeAliases = typeAliases1,
+      opaques = opaques1,
+      specs = specs1,
+    )
+  }
+
+  private def normalizeType(program: S.Program)(tp: S.Type): S.Type = tp match {
+    case S.WildTypeVar() | S.TypeVar(_) => tp
+    case S.TupleType(ts) =>
+      S.TupleType(ts.map(normalizeType(program)))(tp.p)
+    case S.RecordType(fields) =>
+      S.RecordType(fields.map(f => S.Field(f.label, normalizeType(program)(f.value))))(tp.p)
+    case S.OpenRecordType(fields, rt) =>
+      S.OpenRecordType(fields.map(f => S.Field(f.label, normalizeType(program)(f.value))), rt)(tp.p)
+    case S.FunType(args, res) =>
+      S.FunType(args.map(normalizeType(program)), normalizeType(program)(res))(tp.p)
+    case S.ListType(et) =>
+      S.ListType(normalizeType(program)(et))(tp.p)
+    case S.UserType(tName, ts) =>
+      val arity = ts.size
+      val name1 = tName match {
+        case S.LocalName(n) =>
+          val t = new S.LocalFunName(n, arity)
+          program.importTypes.get(t) match {
+            case Some(rem) =>
+              S.RemoteName(rem.module, rem.name)
+            case None =>
+              tName
+          }
+        case S.RemoteName(module, name) =>
+          if (module == program.module) S.LocalName(name)
+          else tName
+      }
+      S.UserType(name1, ts.map(normalizeType(program)))(tp.p)
+  }
+
+  private def normFunType(program: S.Program, tp: S.FunType): S.FunType = {
+    val S.FunType(args, res) = tp
+    S.FunType(args.map(normalizeType(program)), normalizeType(program)(res))(tp.p)
+  }
+
+  def getDeps(program: S.Program): Set[String] = {
+    var result = Set.empty[String]
+    result ++= getDepsRequire(program.require)
+    program.enumDefs.foreach {
+      result ++= getDepEnumDef(_)
+    }
+    program.typeAliases.foreach {
+      result ++= getDepTypeAlias(_)
+    }
+    program.opaques.foreach {
+      result ++= getDepOpaque(_)
+    }
+    program.specs.foreach {
+      result ++= getDepSpec(_)
+    }
+    result ++= getDepImports(program.imports)
+    result ++= getDepImports(program.importTypes)
+    program.funs.foreach {
+      result ++= getDepFun(_)
+    }
+    result
+  }
+
+  private def getDepsRequire(req: S.Require): Set[String] =
+    req.modules.toSet
+
+  private def getDepEnumDef(enumDef: S.EnumDef): Set[String] =
+    enumDef.cons.flatMap(_.argTypes).map(getDepType).foldLeft(Set.empty[String])(_ ++ _)
+
+  private def getDepTypeAlias(typeAlias: S.TypeAlias): Set[String] =
+    getDepType(typeAlias.body)
+
+  private def getDepOpaque(opaque: S.Opaque): Set[String] =
+    getDepType(opaque.body)
+
+  private def getDepSpec(spec: S.Spec): Set[String] =
+    getDepType(spec.funType)
+
+  private def getDepImports(imports: Map[S.LocalFunName, S.RemoteFunName]): Set[String] =
+    imports.values.map(_.module).toSet
+
+  private def getDepFun(fun: S.Fun): Set[String] =
+    fun.clauses.map(getDepClause).foldLeft(Set.empty[String])(_ ++ _)
+
+  private def getDepClause(clause: S.Clause): Set[String] = {
+    var result = Set.empty[String]
+    clause.pats.foreach{
+      result ++= getDepPat(_)
+    }
+    clause.guard.foreach{
+      result ++= getDepExp(_)
+    }
+    result ++= getDepBody(clause.exp)
+    result
+  }
+
+  private def getDepBody(body: S.Body): Set[String] =
+    body.prelude.map(getDepValDef).foldLeft(getDepValDef(body.main))(_ ++ _)
+
+  private def getDepValDef(valDef: S.ValDef): Set[String] =
+    getDepPat(valDef.pat) ++ getDepExp(valDef.exp)
+
+  private def getDepPat(pat: S.Pat): Set[String] =
+    pat match {
+      case S.WildPat() =>
+        Set.empty[String]
+      case S.VarPat(v) =>
+        Set.empty[String]
+      case S.TuplePat(pats) =>
+        pats.map(getDepPat).foldLeft(Set.empty[String])(_ ++ _)
+      case S.RecordPat(fields, _) =>
+        fields.map(f => getDepPat(f.value)).foldLeft(Set.empty[String])(_ ++ _)
+      case S.AndPat(p1, p2) =>
+        getDepPat(p1) ++ getDepPat(p2)
+      case S.EnumCtrPat(enumName, _, pats) =>
+        val ctrDep = enumName match {
+          case S.LocalName(_) =>
+            Set.empty[String]
+          case S.RemoteName(module, _) =>
+            Set(module)
+        }
+        pats.map(getDepPat).foldLeft(ctrDep)(_ ++ _)
+      case S.ListPat(pats) =>
+        pats.map(getDepPat).foldLeft(Set.empty[String])(_ ++ _)
+      case S.ConsPat(hPat, tPat) =>
+        getDepPat(hPat) ++ getDepPat(tPat)
+      case S.BoolPat(_) | S.NumberPat(_) | S.StringPat(_) =>
+        Set.empty[String]
+    }
+
+  private def getDepExp(expr: S.Exp): Set[String] = expr match {
+    case S.IfExp(exp1, exp2, exp3) =>
+      getDepExp(exp1) ++ getDepExp(exp2) ++ getDepExp(exp3)
+    case S.RecordUpdateExp(exp, delta) =>
+      getDepExp(exp) ++ getDepExp(delta)
+    case S.BinOpExp(binOp, exp1, exp2) =>
+      getDepExp(exp1) ++ getDepExp(exp2)
+    case S.UOpExp(uOp, exp) =>
+      getDepExp(exp)
+    case S.AppExp(head, args) =>
+      getDepExp(head) ++ args.flatMap(getDepExp)
+    case S.SelExp(exp, label) =>
+      getDepExp(exp)
+    case S.BoolExp(bool) =>
+      Set.empty
+    case S.NumberExp(n) =>
+      Set.empty
+    case S.CharExp(_) =>
+      Set.empty
+    case S.StringExp(s) =>
+      Set.empty
+    case S.VarExp(_: S.LocalVarName) =>
+      Set.empty
+    case S.VarExp(_: S.LocalFunName) =>
+      Set.empty
+    case S.VarExp(remote: S.RemoteFunName) =>
+      Set(remote.module)
+    case S.RecordExp(fields) =>
+      fields.flatMap(f => getDepExp(f.value)).toSet
+    case S.TupleExp(elems) =>
+      elems.flatMap(getDepExp).toSet
+    case S.EnumConExp(enumName, dataCon, args) =>
+      val ctrDep = enumName match {
+        case S.LocalName(_) =>
+          Set.empty[String]
+        case S.RemoteName(module, _) =>
+          Set(module)
+      }
+      args.map(getDepExp).foldLeft(ctrDep)(_ ++ _)
+    case S.ListExp(elems) =>
+      elems.flatMap(getDepExp).toSet
+    case S.ConsExp(h, t) =>
+      getDepExp(h) ++ getDepExp(t)
+    case S.CaseExp(selector, rules) =>
+      val rulesDeps = rules.flatMap { rule =>
+        val patDeps = getDepPat(rule.pat)
+        val bodyDeps = getDepBody(rule.exp)
+        bodyDeps ++ patDeps
+      }
+      val selectorDeps = getDepExp(selector)
+      selectorDeps ++ rulesDeps
+    case S.FnExp(clauses) =>
+      clauses.map(getDepClause).reduce(_ ++ _)
+    case S.NamedFnExp(varName, clauses) =>
+      clauses.map(getDepClause).reduce(_ ++ _)
+    case S.BlockExpr(body) =>
+      getDepBody(body)
+  }
+
+  private def getDepType(tp: S.Type): Set[String] =
+    tp match {
+      case S.WildTypeVar() | S.TypeVar(_) =>
+        Set.empty[String]
+      case S.TupleType(params) =>
+        params.map(getDepType).foldLeft(Set.empty[String])(_ ++ _)
+      case S.RecordType(fields) =>
+        fields.map(f => getDepType(f.value)).foldLeft(Set.empty[String])(_ ++ _)
+      case S.OpenRecordType(fields, extType) =>
+        fields.map(f => getDepType(f.value)).foldLeft(getDepType(extType))(_ ++ _)
+      case S.FunType(argTypes, resType) =>
+        argTypes.map(getDepType).foldLeft(getDepType(resType))(_ ++ _)
+      case S.ListType(elemType) =>
+        getDepType(elemType)
+      case S.UserType(name, params) =>
+        val nameDep = name match {
+          case S.LocalName(_) =>
+            Set.empty[String]
+          case S.RemoteName(module, _) =>
+            Set(module)
+        }
+        params.map(getDepType).foldLeft(nameDep)(_ ++ _)
+    }
 }
