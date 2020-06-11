283d1c7b0e24046fd39866fcc48c029fa4bfb7ed
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-06-11 12:04:09 +0100

    analyser/Behaviours: aggregated data on modules vs modules with behaviours

--- analyzer/src/main/scala/com/whatsapp/analyzer/Behaviours.scala
+++ analyzer/src/main/scala/com/whatsapp/analyzer/Behaviours.scala
@@ -2,6 +2,9 @@ package com.whatsapp.analyzer
 
 object Behaviours {
 
+  case class AppInfo(name: String, modules: Int, modulesWithBehaviours: Int, usage: Map[String, Int])
+  case class InfoAcc(modules: Int, modulesWithBehaviours: Int, usage: Map[String, Int])
+
   def main(args: Array[String]): Unit = {
     val rpc = RPC.connect()
     val data =
@@ -11,29 +14,36 @@ object Behaviours {
         rpc.close()
       }
 
-    val behaviours = data.keys.toList.sorted
+    val behaviours = data.usage.keys.toList.sorted
     Console.println("ALL")
     for (behaviour <- behaviours) {
-      Console.println(s"$behaviour ${data(behaviour)}")
+      Console.println(s"$behaviour ${data.usage(behaviour)}")
     }
+
+    Console.println(s"Modules:    ${data.modules}")
+    Console.println(s"Behaviours: ${data.modulesWithBehaviours}")
     Console.println("Top10")
-    val top10 = data.toList.sortBy(_._2).reverse.take(10)
+    val top10 = data.usage.toList.sortBy(_._2).reverse.take(10)
     for ((behaviour, count) <- top10) {
       Console.println(s"$behaviour $count")
     }
   }
 
-  private def loadData(rpc: RPC): Map[String, Int] = {
-    var usage = Map.empty[String, Int].withDefaultValue(0)
+  private def loadData(rpc: RPC): InfoAcc = {
+    var usageAcc = Map.empty[String, Int].withDefaultValue(0)
+    var modulesAcc = 0
+    var modulesWithBehavioursAcc = 0
     val projectUsages = CodeDirs.projectEbinDirs.map(indexProjectDir(_, rpc))
-    for {
-      (libName, x) <- projectUsages if !CodeDirs.thirdParty.contains(libName)
-      (behaviour, count) <- x
-    } usage = usage.updated(behaviour, usage(behaviour) + count)
-    usage
+    for (AppInfo(libName, modules, modulesWithBehaviours, usage) <- projectUsages if !CodeDirs.thirdParty.contains(libName)) {
+      modulesAcc = modulesAcc + modules
+      modulesWithBehavioursAcc = modulesWithBehavioursAcc + modulesWithBehaviours
+      for ((behaviour, count) <- usage)
+        usageAcc = usageAcc.updated(behaviour, usageAcc(behaviour) + count)
+    }
+    InfoAcc(modulesAcc, modulesWithBehavioursAcc, usageAcc)
   }
 
-  private def indexProjectDir(dir: String, rpc: RPC): (String, Map[String, Int]) = {
+  private def indexProjectDir(dir: String, rpc: RPC): AppInfo = {
     val start = dir.lastIndexOf("/lib/")
     val end = dir.lastIndexOf('/')
     val libName = dir.substring(start + 5, end)
@@ -42,13 +52,19 @@ object Behaviours {
     val moduleNames = beamFiles.map(s => s.substring(0, s.length - 5)).sorted
 
     var usage = Map.empty[String, Int].withDefaultValue(0)
+    var modules: Int = 0
+    var modulesWithBehaviours: Int = 0
     for (mName <- moduleNames) {
       val path = s"$dir/${mName}.beam"
       val Some(behaviours) = rpc.getBehaviours(path)
       for (behaviour <- behaviours) {
         usage = usage.updated(behaviour, usage(behaviour) + 1)
       }
+      modules = modules + 1
+      if (behaviours.nonEmpty) {
+        modulesWithBehaviours = modulesWithBehaviours + 1
+      }
     }
-    (libName, usage)
+    AppInfo(libName, modules, modulesWithBehaviours, usage)
   }
 }
