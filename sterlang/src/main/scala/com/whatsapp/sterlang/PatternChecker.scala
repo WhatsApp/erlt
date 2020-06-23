704c74a6ad6283f2cb58280a88818d0515bd1038
cacay <coskuacay@gmail.com>
2020-06-23 15:29:08 -0400

    Delete accidentally added file

--- sterlang/src/main/scala/com/whatsapp/sterlang/PatternChecker.scala
+++
@@ -1,8 +0,0 @@
-package com.whatsapp.sterlang
-
-/** Generates warnings for missing and redundant
-  *
-  * Checks uses of pattern matching for exhaustiveness and conciseness,
-  * and generates warnings for missing and redundant cases.
-  */
-object PatternChecker {}
