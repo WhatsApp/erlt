81cb00031abfd9450062aff137d79183b4a562f1
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-07-11 14:15:09 +0100

    forms: sp utility

---
+++ sterlang/src/main/scala/com/whatsapp/sterlang/forms/package.scala
@@ -0,0 +1,23 @@
+package com.whatsapp.sterlang
+
+import com.whatsapp.sterlang.etf._
+
+package object forms {
+  def sp(term: ETerm): Pos.SP =
+    term match {
+      case ETuple(
+            List(
+              ETuple(List(ELong(line1), ELong(col1))),
+              ETuple(List(ELong(line2), ELong(col2))),
+            )
+          ) =>
+        val loc1 = Pos.Loc(line1.toInt, col1.toInt)
+        val loc2 = Pos.Loc(line2.toInt, col2.toInt)
+        assert(loc1 != Pos.Loc(0, 0), term.toString)
+        assert(loc2 != Pos.Loc(0, 0), term.toString)
+        assert(loc1 != loc2, term.toString)
+        Pos.SP(loc1, loc2)
+      case _ =>
+        sys.error(s"cannot parse location: $term")
+    }
+}
