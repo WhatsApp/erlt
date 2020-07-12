b4182c2b9e6b352312db9d5e67f47f55c7436987
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-07-12 18:12:25 +0100

    sterlang scala parsing: ##{} for open record patterns

--- sterlang/examples/pos/tutorial.erl
+++ sterlang/examples/pos/tutorial.erl
@@ -67,7 +67,7 @@ pieces() ->
     #{n := N1, st := S1, z := Z1} = My_rec,
     #{n := N, st := St, z := Z} = My_rec,
     Rec1 = #{n => N, z => Z},
-    #{n := NVal, ... } = My_rec,
+    ##{n := NVal} = My_rec,
     % transforming record:
     My_rec1 = My_rec #{n := 2},
     My_pair = {1, []},
@@ -119,7 +119,7 @@ get_id(R) ->
 
 % So, what to do if we want to process "heterogeneous" records together?
 % Explicitly cast/restrict them!
-extract(#{id := Id, ...}) -> #{id => Id}.
+extract(##{id := Id}) -> #{id => Id}.
 
 records_example() ->
     % polymorphism - get_id is polymorphic
@@ -138,11 +138,11 @@ records_example() ->
 
 % record is a special "kind" of types
 id_rec(R) ->
-    #{...} = R,
+    ##{} = R,
     R.
 
 get_id1(Rec) ->
-    #{id := IdVal, ...} = Rec,
+    ##{id := IdVal} = Rec,
     IdVal.
 
 update_a_b(R) ->
