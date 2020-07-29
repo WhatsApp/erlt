bc9532bce552fd1da72cb632def803666708547e
cacay <coskuacay@gmail.com>
2020-07-29 18:53:52 -0400

    sterlang: add pattern matching tests for records

---
+++ sterlang/examples/pattern-error/inexhaustive_record2.erl
@@ -0,0 +1,20 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-lang([erl2, st]).
+-module(inexhaustive_record2).
+-export([test1/1]).
+
+-spec test1(#{a := boolean(), b := boolean()}) -> {}.
+test1(##{a := false}) -> {}.
