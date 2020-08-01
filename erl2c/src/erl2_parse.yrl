WhatsApp/erlt

commit 0c9347ad15e049c51f4ef1e6e44f0b63ee78b5a3

stErlang: OCaml-like exceptions (#120)

erl2c/src/erl2_parse.yrl
@@ -1222,6 +1222,11 @@ build_typed_attribute(
 ) ->
     {attribute, Aa, record, {RecordName, record_tuple(RecTuple)}};
 build_typed_attribute(
+    {atom, Aa, exception},
+    {typed_record, _TRA, {atom, _An, RecordName}, RecTuple}
+) ->
+    {attribute, Aa, exception, {RecordName, record_tuple(RecTuple)}};
+build_typed_attribute(
     {atom, Aa, record},
     {typed_record, _TRA, {remote, _, {atom, MRa, Name1}, {atom, _, Name2}}, RecTuple}
 ) ->
@@ -1252,6 +1257,7 @@ build_typed_attribute(
 build_typed_attribute({atom, Aa, Attr}, _) ->
     case Attr of
         record -> error_bad_decl(Aa, record);
+        exception -> error_bad_decl(Aa, exception);
         type -> error_bad_decl(Aa, type);
         opaque -> error_bad_decl(Aa, opaque);
         enum -> error_bad_decl(Aa, enum);
@@ -1385,6 +1391,13 @@ build_attribute({atom, Aa, import_type}, Val) ->
             {attribute, Aa, import_type, {Mod, farity_list(ImpList)}};
         _Other ->
             error_bad_decl(Aa, import_type)
+    end;
+build_attribute({atom, Aa, exception}, Val) ->
+    case Val of
+        [{atom, _An, Record}, RecTuple] ->
+            {attribute, Aa, exception, {Record, record_tuple(RecTuple)}};
+        _Other ->
+            error_bad_decl(Aa, record)
     end;
 build_attribute({atom, Aa, record}, Val) ->
     case Val of
@@ -2006,6 +2019,10 @@ modify_anno1({attribute, A, record, {Name, Fields}}, Ac, Mf) ->
     {A1, Ac1} = Mf(A, Ac),
     {Fields1, Ac2} = modify_anno1(Fields, Ac1, Mf),
     {{attribute, A1, record, {Name, Fields1}}, Ac2};
+modify_anno1({attribute, A, exception, {Name, Fields}}, Ac, Mf) ->
+    {A1, Ac1} = Mf(A, Ac),
+    {Fields1, Ac2} = modify_anno1(Fields, Ac1, Mf),
+    {{attribute, A1, exception, {Name, Fields1}}, Ac2};
 modify_anno1({attribute, A, spec, {Fun, Types}}, Ac, Mf) ->
     {A1, Ac1} = Mf(A, Ac),
     {Types1, Ac2} = modify_anno1(Types, Ac1, Mf),

erl2c/src/erl2_compile.erl
@@ -170,6 +170,7 @@ do_file(File, Options0) ->
                     ?pass(parse_module),
                     ?pass(check_parse_errors),
                     ?pass(extract_options),
+                    ?pass(erl2_exception),
                     ?pass(erl2_module_record),
                     ?pass(erl2_lint),
                     ?pass(erl2_expand),
@@ -188,6 +189,7 @@ do_file(File, Options0) ->
                     ?pass(parse_module),
                     ?pass(check_parse_errors),
                     ?pass(extract_options),
+                    ?pass(erl2_exception),
                     ?pass(erl2_module_record),
                     ?pass(erl2_lint),
                     ?pass(erl2_expand),
@@ -205,6 +207,7 @@ do_file(File, Options0) ->
                     ?pass(parse_module),
                     ?pass(check_parse_errors),
                     ?pass(extract_options),
+                    ?pass(erl2_exception),
                     ?pass(erl2_module_record),
                     ?pass(erl2_lint),
                     ?pass(erl2_expand),
@@ -222,6 +225,7 @@ do_file(File, Options0) ->
                     ?pass(parse_module),
                     ?pass(check_parse_errors),
                     ?pass(extract_options),
+                    ?pass(erl2_exception),
                     ?pass(erl2_module_record),
                     ?pass(erl2_lint),
                     ?pass(erl2_expand),
@@ -996,6 +1000,16 @@ erl2_module_record(Code, St) ->
         false ->
             {ok, Code, St}
     end.
+
+erl2_exception(Code, St) ->
+    case is_lang_erl2(St) of
+        true ->
+            Code1 = erl2_exception:parse_transform(Code, St#compile.options),
+            {ok, Code1, St};
+        false ->
+            {ok, Code, St}
+    end.
+
 
 erl2_expand(Code, St) ->
     case is_lang_erl2(St) of

erl2c/src/erl2_exception.erl
@@ -1,0 +1,32 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(erl2_exception).
+
+-export([parse_transform/2]).
+
+parse_transform(Forms, _Options) ->
+    forms(Forms).
+
+forms([F0 | Fs0]) ->
+    F1 = form(F0),
+    Fs1 = forms(Fs0),
+    [F1 | Fs1];
+forms([]) ->
+    [].
+
+form({attribute, Anno, exception, Def}) ->
+    {attribute, Anno, record, Def};
+form(Form) ->
+    Form.

erl2c/src/erl2_expand.erl
@@ -163,6 +163,8 @@ type({ann_type, L, [Var, Type]}, St) ->
 type({remote_type, L, [M, F, Args]}, St) ->
     {Args1, St1} = type_list(Args, St),
     {{remote_type, L, [M, F, Args1]}, St1};
+type({user_type, L, exception, []}, St) ->
+    {{type, L, any, []}, St};
 type({user_type, L, TypeName, Args}, St) ->
     {Args1, St1} = type_list(Args, St),
     TA = {TypeName, length(Args)},

erl2c/src/erl2_lint.erl
@@ -3803,6 +3803,8 @@ used_type(TypePair, L, #lint{usage = Usage, file = File} = St) ->
     UsedTypes = dict:store(TypePair, erl_anno:set_file(File, L), OldUsed),
     St#lint{usage = Usage#usage{used_types = UsedTypes}}.
 
+is_default_type({exception, 0}) ->
+    true;
 is_default_type({Name, NumberOfTypeVariables}) ->
     erl_internal:is_type(Name, NumberOfTypeVariables).
 

examples/dev/ir-spec/exn.P
@@ -1,0 +1,22 @@
+-file("exn.erl", 1).
+
+-module(exn).
+
+-export([]).
+
+-record(error1,{}).
+
+-record(error2,{name :: string()}).
+
+-type my_error() :: {969696, exn, my_error, my_error, any()}.
+
+mk_error1() ->
+    {error1}.
+
+-spec mk_error2() -> any().
+
+mk_error2() ->
+    {error2, "bad error"}.
+
+
+

examples/dev/src/exn.erl
@@ -1,0 +1,31 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-lang([erl2]).
+-module(exn).
+
+-export([]).
+
+-exception(error1, {}).
+-exception(error2, {name :: string()}).
+
+-enum my_error() :: my_error{exception()}.
+
+mk_error1() ->
+    #error1{}.
+
+-spec mk_error2() -> exception().
+mk_error2() ->
+    #error2{name = "bad error"}.
+

sterlang/examples/dev/exceptions.erl
@@ -1,0 +1,51 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-lang([erl2, st]).
+-module(exceptions).
+
+-exception(not_found, {}).
+-exception(invocation_target_exception, {target :: exception()}).
+-exception(message_error, {message :: string()}).
+
+-enum option(A) :: none{} | some{A}.
+
+mk_exn(option.none{}) ->
+    #not_found{};
+mk_exn(option.some{Target}) ->
+    #invocation_target_exception{target = Target}.
+
+get_target(#not_found{}) ->
+    option.none{};
+get_target(#invocation_target_exception{target = Target}) ->
+    option.some{Target}.
+
+get_message(M) ->
+    try
+        M()
+    catch
+        #message_error{message = Msg} -> Msg;
+        _ -> "Sorry"
+    end.
+
+get_other(F, G) ->
+    try
+        F(1)
+    of
+        {Res} -> Res
+    catch
+        #message_error{message = Msg} -> Msg
+    after
+        F(G)
+    end.

commit 541d8ef3175e72ffb7544c16c43b9d2b1f9bcb37

Fix typo
Was introduced in 0c9347ad15e049c51f4ef1e6e44f0b63ee78b5a3

erl2c/src/erl2_parse.yrl
@@ -1397,7 +1397,7 @@ build_attribute({atom, Aa, exception}, Val) ->
         [{atom, _An, Record}, RecTuple] ->
             {attribute, Aa, exception, {Record, record_tuple(RecTuple)}};
         _Other ->
-            error_bad_decl(Aa, record)
+            error_bad_decl(Aa, exception)
     end;
 build_attribute({atom, Aa, record}, Val) ->
     case Val of