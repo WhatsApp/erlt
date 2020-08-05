16cb3e53bf356f2c399e281a96e7367f7b13c040
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-08-05 10:05:34 +0100

    some initial example

---
+++ examples/enum_interop/src/interop01.erl
@@ -0,0 +1,26 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-lang([erl2]).
+-module(interop01).
+
+-enum box(A, B)  :: box1{E@1 :: A}
+                  | box2{E@2 :: B}.
+-type box@(A, B) :: {box1, E@1 :: A}
+                  | {box2, E@2 :: B}.
+
+-enum choice(A, B)  :: choice1{E@1 :: A}
+                     | choice2{E@2 :: A, E@2 :: B}.
+-type choice@(A, B) :: {E@1 :: A}
+                     | {E@2 :: A, E@1 :: B}.
