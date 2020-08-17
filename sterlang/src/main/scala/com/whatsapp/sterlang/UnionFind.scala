7adce6262ec6c254e5a7646e8fcca5828c36a3b3
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-08-17 21:48:05 +0100

    stErlang: classic UnionFind

--- sterlang/src/main/scala/com/whatsapp/sterlang/UnionFind.scala
+++ sterlang/src/main/scala/com/whatsapp/sterlang/UnionFind.scala
@@ -16,56 +16,75 @@
 
 package com.whatsapp.sterlang
 
-// Naive implementation of UnionFind
 object UnionFind {
-  class Var[A](private[UnionFind] var rank: Int) {
-    override def toString: String = s"Var[rank=$rank]"
-  }
-}
-class UnionFind[A] {
-  import scala.collection.mutable.{ArrayBuffer, Map => MMap}
-  import UnionFind._
+  class Var[A](private[UnionFind] var x: URefC[A])
 
-  private var nextRank = 0
-  private val rankToValue: MMap[Int, A] = MMap()
-  private val vars: ArrayBuffer[Var[A]] = ArrayBuffer()
+  // Pointer (PTR) or Equivalence Class Representative (ECR)
+  private sealed trait URefC[A]
+  private case class ECR[A](var a: (A, Int)) extends URefC[A]
+  private case class PTR[A](var ref: Var[A]) extends URefC[A]
 
-  private def nextTRank(): Int = {
-    nextRank = nextRank + 1
-    nextRank - 1
-  }
+  private[UnionFind] def uRef[A](a: (A, Int)): Var[A] =
+    new Var(ECR(a))
 
-  def make(value: A): Var[A] = {
-    val tVar = new Var[A](nextTRank())
-    vars.append(tVar)
-    rankToValue.put(tVar.rank, value)
-    tVar
-  }
+  private[UnionFind] def find[A](uref: Var[A]): Var[A] =
+    uref.x match {
+      case ECR(_) =>
+        uref
+      case PTR(p1) =>
+        val p2 = find(p1)
+        uref.x = PTR(p2)
+        p2
+    }
 
-  def get(v: Var[A]): A =
-    rankToValue(v.rank)
-  def set(v: Var[A], value: A): Unit =
-    rankToValue.put(v.rank, value)
-  def eq(v1: Var[A], v2: Var[A]): Boolean =
-    v1.rank == v2.rank
-  def id(v: Var[A]): Int =
-    v.rank
-
-  def link(v1: Var[A], v2: Var[A]): Boolean =
-    if (v1.rank == v2.rank) {
+  private[UnionFind] def _link[A](p: Var[A], q: Var[A]): Boolean = {
+    val p1 = find(p)
+    val q1 = find(q)
+    if (p1 eq q1) {
       false
     } else {
-      val (rank1, rank2) = (v1.rank, v2.rank)
-      val rank = nextTRank()
-      rankToValue.put(rank, rankToValue(rank2))
-
-      vars.foreach { v =>
-        if (v.rank == rank1 || v.rank == rank2)
-          v.rank = rank
-      }
-
-      rankToValue.remove(rank1)
-      rankToValue.remove(rank2)
+      p1.x = PTR(q)
       true
     }
+  }
+
+  private[UnionFind] def _eq[A](p: Var[A], q: Var[A]): Boolean =
+    find(p) eq find(q)
+
+  private[UnionFind] def _get[A](p: Var[A]): (A, Int) = {
+    val ECR(a) = find(p).x
+    a
+  }
+
+  private[UnionFind] def _set[A](p: Var[A], x: (A, Int)): Unit = {
+    val t @ ECR(_) = find(p).x
+    t.a = x
+  }
+}
+
+final class UnionFind[A] {
+  import UnionFind._
+
+  private var tNext = 0
+
+  def make(a: A): Var[A] = {
+    val i = tNext
+    tNext = tNext + 1
+    UnionFind.uRef((a, i))
+  }
+
+  def get(tv: Var[A]): A =
+    UnionFind._get(tv)._1
+
+  def set(tv: Var[A], a: A): Unit =
+    UnionFind._set(tv, (a, id(tv)))
+
+  def id(tv: Var[A]): Int =
+    UnionFind._get(tv)._2
+
+  def eq(tv1: Var[A], tv2: Var[A]): Boolean =
+    UnionFind._eq(tv1, tv2)
+
+  def link(tv1: Var[A], tv2: Var[A]): Boolean =
+    UnionFind._link(tv1, tv2)
 }
