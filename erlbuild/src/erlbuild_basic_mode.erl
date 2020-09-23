504991e770eb61dfbac4cb1ec58b246f1ca0b6be
Maxwell Elliot Heiber <mheiber@fb.com>
2020-09-23 10:21:34 +0100

    try link and list-comprehension based p_each

--- erlbuild/src/erlbuild_basic_mode.erl
+++ erlbuild/src/erlbuild_basic_mode.erl
@@ -16,9 +16,9 @@ invoke(#args{command = clean, build_dir = undefined}) ->
 invoke(#args{command = clean, build_dir = BuildDir}) ->
     file:del_dir_r(BuildDir).
 
-% calls erltc Phase on InputFiles in parallel 
+% calls erltc Phase on InputFiles in parallel
 do_phase(Phase, #args{input_files = InputFiles} = Args) ->
-    pmap(
+    p_each(
         fun(InputFile) -> do_file(Phase, Args, InputFile) end,
         Phase,
         InputFiles,
@@ -52,27 +52,24 @@ do_file(
 
 %% UTILITIES
 
-% Errors are not handled (nocatch) - they take down this process and a stack is dumped
-pmap(_, _, [], _, _) ->
+% Errors are not handled (nocatch)
+% and we ignore return values
+p_each(_, _, [], _, _) ->
     ok;
-pmap(F, DisplayName, List, Concurrency, TimeoutForFile) ->
+p_each(F, DisplayName, List, Concurrency, TimeoutForFile) ->
     {L1, L2} = safe_split(Concurrency, List),
-    [spawn_monitor(fun() -> F(Item) end) || Item <- L1],
-    loop_monitor(DisplayName, TimeoutForFile, L1),
-    pmap(F, DisplayName, L2, Concurrency, TimeoutForFile).
-
-% receive 1 'DOWN' for each item in the H :: T
-loop_monitor(_, _, []) ->
-    ok;
-loop_monitor(DisplayName, TimeoutForFile, [H | T]) ->
-    receive
-        {'DOWN', _, _, _, _} ->
-            loop_monitor(DisplayName, TimeoutForFile, T);
-        Unknown ->
-            erlbuild_util:throw_error("received unknown message ~p~n", [Unknown])
-    after TimeoutForFile ->
-        erlbuild_util:throw_error("timed out waiting for ~p(~p)~n", [DisplayName, H])
-    end.
+    Pid = self(),
+    [spawn_link(fun() -> Pid ! {Item, F(Item)} end) || Item <- L1],
+    [
+        receive
+            {Item, _Res} ->
+                ok
+        after TimeoutForFile ->
+            erlbuild_util:throw_error("timed out waiting for ~p of ~p ~n", [DisplayName, Item])
+        end
+        || Item <- L1
+    ],
+    p_each(F, DisplayName, L2, Concurrency, TimeoutForFile).
 
 concurrency() ->
     case erlang:system_info(logical_processors) of
