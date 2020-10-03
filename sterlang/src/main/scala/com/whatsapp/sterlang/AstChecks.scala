4bc79de0d5b9015274d31f242b1e4d218fd440e0
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-10-03 12:37:54 +0100

    structs: checking usage of type parameters

--- sterlang/src/main/scala/com/whatsapp/sterlang/AstChecks.scala
+++ sterlang/src/main/scala/com/whatsapp/sterlang/AstChecks.scala
@@ -150,10 +150,12 @@ class AstChecks(val context: Context) {
       }
       fieldNames = fieldNames + f.label
     }
+    val bound = collectParams(structDef.params)
     structDef.fields.foreach { f =>
       expandType(program, Set.empty)(f.value)
-      collectRHSTypeVars(Set.empty)(f.value)
     }
+    val used = structDef.fields.map { f => collectRHSTypeVars(bound)(f.value) }.foldLeft(Set.empty[TypeVar])(_ ++ _)
+    checkUsage(structDef.params, used)
   }
 
   private def expandType(program: Program, visited: Set[LocalName])(tp: Type): Unit =
@@ -179,9 +181,9 @@ class AstChecks(val context: Context) {
                     params.foreach(expandType(program, visited))
                   case None =>
                     val strName = name.stringId
-                    // TODO - params
-                    program.structDefs.find(_.name == strName) match {
+                    program.structDefs.find(s => s.name == strName && s.params.size == params.size) match {
                       case Some(eRec) =>
+                        params.foreach(expandType(program, visited))
                         eRec.kind match {
                           case ExnStruct =>
                             throw new ExceptionType(tp.r, strName)
