85deb4a8327f56f61a0dd4608cf1cb99f712f47d
Michał Muskała <micmus@whatsapp.com>
2020-10-13 21:28:04 +0100

    Properly handle pins in catch patterns (#244)
    We now parse all expressions in catch & handle reporting invalid
    ones from erlt_lint, this should result in better error messages,
    and simpler parser, the extra complexity in erlt_lint is minimal.
    
    This also contains a fix to the OTP bug discovered during the implementation
    https://bugs.erlang.org/browse/ERL-1380.
    
    #erlt_lint:lint is simplified by dropping in_try_head boolean variable from
    the state - it's not needed after refactoring.

--- erltc/src/erlt_lint.erl
+++ erltc/src/erlt_lint.erl
@@ -238,8 +238,6 @@ value_option(Flag, Default, On, OnVal, Off, OffVal, Opts) ->
     enums = #{},
     %Current enum
     enum = [] :: atom() | [],
-    %In a try head.
-    in_try_head = false :: boolean(),
     % Global definition database
     defs_db :: erlt_defs:defs(),
     % Used to track recursive struct defaults
@@ -468,6 +466,10 @@ format_error({stacktrace_guard, V}) ->
     io_lib:format("stacktrace variable ~w must not be used in a guard", [V]);
 format_error({stacktrace_bound, V}) ->
     io_lib:format("stacktrace variable ~w must not be previously bound", [V]);
+format_error(illegal_catch_kind) ->
+    "only variables and atoms throw, exit, and error are allowed in kind position of catch clauses";
+format_error(illegal_catch_stack) ->
+    "only unbound variables are allowed in stacktrace position of catch clauses";
 %% --- binaries ---
 format_error({undefined_bittype, Type}) ->
     io_lib:format("bit type ~tw undefined", [Type]);
@@ -3896,11 +3898,10 @@ is_module_dialyzer_option(Option) ->
 
 try_clauses(Scs, Ccs, In, Vt, St0) ->
     {Csvt0, St1} = icrt_clauses(Scs, Vt, St0),
-    St2 = St1#lint{in_try_head = true},
-    {Csvt1, St3} = icrt_clauses(Ccs, Vt, St2),
+    {Csvt1, St2} = catch_clauses(Ccs, Vt, St1),
     Csvt = Csvt0 ++ Csvt1,
-    UpdVt = icrt_export(Csvt, Vt, In, St3),
-    {UpdVt, St3#lint{in_try_head = false}}.
+    UpdVt = icrt_export(Csvt, Vt, In, St2),
+    {UpdVt, St2}.
 
 %% icrt_clauses(Clauses, In, ImportVarTable, State) ->
 %%      {UpdVt,State}.
@@ -3916,35 +3917,49 @@ icrt_clauses(Cs, In, Vt, St0) ->
 icrt_clauses(Cs, Vt, St) ->
     mapfoldl(fun(C, St0) -> icrt_clause(C, Vt, St0) end, St, Cs).
 
-icrt_clause({clause, _Line, H, G, B}, Vt0, St0) ->
-    Vt1 = taint_stack_var(Vt0, H, St0),
-    {Hvt, Binvt, St1} = head(H, Vt1, St0),
-    Vt2 = vtupdate(Hvt, Binvt),
-    St1_1 = unpinned_vars(Binvt, Vt0, St1),
-    Vt3 = taint_stack_var(Vt2, H, St1_1),
-    {Gvt, St2} = guard(G, vtupdate(Vt3, Vt0), St1_1#lint{in_try_head = false}),
-    Vt4 = vtupdate(Gvt, Vt2),
-    {Bvt, St3} = exprs(B, vtupdate(Vt4, Vt0), St2),
-    {vtupdate(Bvt, Vt4), St3}.
+catch_clauses(Cs, Vt, St) ->
+    mapfoldl(fun(C, St0) -> catch_clause(C, Vt, St0) end, St, Cs).
 
-taint_stack_var(Vt, Pat, #lint{in_try_head = true}) ->
-    [{tuple, _, [_, _, {var, _, Stk}]}] = Pat,
-    case Stk of
-        '_' ->
-            Vt;
-        _ ->
-            lists:map(
-                fun
-                    ({V, {bound, Used, Lines}}) when V =:= Stk ->
-                        {V, {stacktrace, Used, Lines}};
-                    (B) ->
-                        B
-                end,
-                Vt
-            )
+catch_clause({clause, _Line, H, G, B}, Vt0, St0) ->
+    {TaintVt, St1} = catch_head(H, Vt0, St0),
+    icrt_clause(H, G, B, Vt0, TaintVt, St1).
+
+icrt_clause({clause, _Line, H, G, B}, Vt0, St0) ->
+    icrt_clause(H, G, B, Vt0, [], St0).
+
+icrt_clause(H, G, B, Vt0, TaintVt, St0) ->
+    {Hvt, Binvt, St1} = head(H, Vt0, TaintVt, St0),
+    Vt1 = vtupdate(Hvt, Binvt),
+    St2 = unpinned_vars(Binvt, vtmerge(TaintVt, Vt0), St1),
+    Vt2 = vtupdate(TaintVt, Vt1),
+    {Gvt, St3} = guard(G, vtupdate(Vt2, Vt0), St2),
+    Vt3 = vtupdate(Gvt, Vt1),
+    {Bvt, St4} = exprs(B, vtupdate(Vt3, Vt0), St3),
+    {vtupdate(Bvt, Vt3), St4}.
+
+catch_head([{tuple, _, [Kind, _Reason, Stack]}], Vt0, St0) ->
+    St1 = validate_catch_kind(Kind, St0),
+    taint_stack_var(Stack, Vt0, St1).
+
+validate_catch_kind({var, _, _}, St) -> St;
+validate_catch_kind({atom, _, exit}, St) -> St;
+validate_catch_kind({atom, _, error}, St) -> St;
+validate_catch_kind({atom, _, throw}, St) -> St;
+validate_catch_kind({op, _, '^', {var, _, _}}, St) -> St;
+validate_catch_kind(Other, St) -> add_error(element(2, Other), illegal_catch_kind, St).
+
+taint_stack_var({var, _, '_'}, _Vt, St) ->
+    {[], St};
+taint_stack_var({var, Loc, Name}, Vt, St) ->
+    TaintVt = [{Name, {stacktrace, unused, [Loc]}}],
+    case orddict:is_key(Name, Vt) of
+        true ->
+            {TaintVt, add_error(Loc, {stacktrace_bound, Name}, St)};
+        false ->
+            {TaintVt, St}
     end;
-taint_stack_var(Vt, _Pat, #lint{in_try_head = false}) ->
-    Vt.
+taint_stack_var(Other, _Vt, St) ->
+    {[], add_error(element(2, Other), illegal_catch_stack, St)}.
 
 icrt_export(Vts, Vt, {Tag, Attrs}, St) ->
     {_File, Loc} = loc(Attrs, St),
@@ -4192,6 +4207,9 @@ pat_var(V, Line, Vt, Bvt, St) ->
                     {[{V, {bound, used, Ls}}], [],
                         %% As this is matching, exported vars are risky.
                         add_warning(Line, {exported_var, V, From}, St)};
+                {ok, {stacktrace, _Usage, [Line]}} ->
+                    %% this is the definitoon & use, all is fine
+                    {[], [{V, {bound, unused, [Line]}}], St};
                 {ok, {stacktrace, _Usage, Ls}} ->
                     {[{V, {bound, used, Ls}}], [], add_error(Line, {stacktrace_bound, V}, St)};
                 error ->
@@ -4258,16 +4276,19 @@ exported_var(Line, V, From, St) ->
     end.
 
 unpinned_vars(Vt, Vt0, St0) ->
-    foldl(
-        fun
-            ({V, {_, _, [L | _]}}, St) ->
+    OldVt = vt_no_unsafe(Vt0),
+    Check = fun({V, {_, _, [L | _]}}, St) ->
+        case orddict:find(V, OldVt) of
+            {ok, {stacktrace, _, _}} ->
+                %% error already reported
+                St;
+            {ok, _} ->
                 add_error(L, {unpinned_var, V}, St);
-            (_, St) ->
+            error ->
                 St
-        end,
-        St0,
-        vtold(Vt, vt_no_unsafe(Vt0))
-    ).
+        end
+    end,
+    foldl(Check, St0, Vt).
 
 shadow_vars(Vt, Vt0, In, St0) ->
     case is_warn_enabled(shadow_vars, St0) of
