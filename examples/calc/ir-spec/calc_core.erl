b9bb820afe160894199b9de595076ca89fdadd1a
Alan Zimmerman <alan.zimm@gmail.com>
2020-10-20 14:08:56 +0100

    Fix the calc example after enum diff landed (#264)
    Paired on with @mheiber 
    Co-authored-by: Michał Muskała <micmus@whatsapp.com>

--- examples/calc/ir-spec/calc_core.erl
+++ examples/calc/ir-spec/calc_core.erl
@@ -11,44 +11,37 @@
          expr_divd/2,
          expr_var/1]).
 
--type expr() :: {969696,
-                 calc_core,
-                 expr,
-                 number,
-                 integer()} |
-                {969696, calc_core, expr, add, expr(), expr()} |
-                {969696, calc_core, expr, subtr, expr(), expr()} |
-                {969696, calc_core, expr, mult, expr(), expr()} |
-                {969696, calc_core, expr, divd, expr(), expr()} |
-                {969696, calc_core, expr, var, atom()}.
+-type expr() :: {'$#calc_core:expr.number', integer()} |
+                {'$#calc_core:expr.add', expr(), expr()} |
+                {'$#calc_core:expr.subtr', expr(), expr()} |
+                {'$#calc_core:expr.mult', expr(), expr()} |
+                {'$#calc_core:expr.divd', expr(), expr()} |
+                {'$#calc_core:expr.var', atom()}.
 
 -spec expr_number(integer()) -> expr().
 
-expr_number(N) -> {969696, calc_core, expr, number, N}.
+expr_number(N) -> {'$#calc_core:expr.number', N}.
 
 -spec expr_add(expr(), expr()) -> expr().
 
-expr_add(E1, E2) ->
-    {969696, calc_core, expr, add, E1, E2}.
+expr_add(E1, E2) -> {'$#calc_core:expr.add', E1, E2}.
 
 -spec expr_subtr(expr(), expr()) -> expr().
 
 expr_subtr(E1, E2) ->
-    {969696, calc_core, expr, subtr, E1, E2}.
+    {'$#calc_core:expr.subtr', E1, E2}.
 
 -spec expr_mult(expr(), expr()) -> expr().
 
-expr_mult(E1, E2) ->
-    {969696, calc_core, expr, mult, E1, E2}.
+expr_mult(E1, E2) -> {'$#calc_core:expr.mult', E1, E2}.
 
 -spec expr_divd(expr(), expr()) -> expr().
 
-expr_divd(E1, E2) ->
-    {969696, calc_core, expr, divd, E1, E2}.
+expr_divd(E1, E2) -> {'$#calc_core:expr.divd', E1, E2}.
 
 -spec expr_var(atom()) -> expr().
 
-expr_var(A) -> {969696, calc_core, expr, var, A}.
+expr_var(A) -> {'$#calc_core:expr.var', A}.
 
 
 
