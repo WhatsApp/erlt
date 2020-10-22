0b8536c31d16b2b12cc2132486fd861e04fcd073
Michał Muskała <micmus@whatsapp.com>
2020-10-22 12:32:53 +0100

    Fix remote enums & structs with positional fields

--- examples/dev_enum/ir-spec/enum_mod12.erl
+++ examples/dev_enum/ir-spec/enum_mod12.erl
@@ -1,18 +1,8 @@
--file("dev_enum/src/enum_mod11.erlt", 1).
+-file("dev_enum/src/enum_mod12.erlt", 1).
 
--module(enum_mod11).
+-module(enum_mod12).
 
 -export([expr/0, pattern/3, guard/3]).
-
--export_type([bar/0, baz/0]).
-
--type bar() :: {'$#enum_mod11:bar.x',
-                integer(),
-                integer()}.
-
--type baz() :: {'$#enum_mod11:baz.x',
-                integer(),
-                integer()}.
 
 expr() ->
     {{'$#enum_mod11:bar.x', 1, 2},
