d67907ffd334b63fb083e5160f6432b652913d6d
Per Gustafsson <pergu@fb.com>
2020-11-13 09:13:56 +0000

    Adds checks that an exported type only refers to other exported types. Also moves the consistency checking of type exports, imports and definitions to this module. This means checking that exported types are defined, imported types do not clash with other imported or defined types. It does not include checking that imported types are used, the reason for this is that imported types which are enums and structs can be used in the expression language.

--- examples/dev_enum/ir-spec/enum_mod03.erl
+++ examples/dev_enum/ir-spec/enum_mod03.erl
@@ -2,11 +2,16 @@
 
 -module(enum_mod03).
 
--export_type([foo/0, bar/1, default_with_default/0]).
+-export_type([foo/0,
+              bar/1,
+              default_with_default/0,
+              opaque_baz/0]).
 
 -export([foo/0, bar/0, baz/0, pattern/3, guard/1]).
 
 -type bar(A) :: {'$#enum_mod03:bar.x', A, foo()}.
+
+-opaque opaque_baz() :: baz().
 
 -type baz() :: {'$#enum_mod03:baz.x', bar(integer())}.
 
@@ -14,7 +19,7 @@
 
 -type
      default_with_default() :: {'$#enum_mod03:default_with_default.x',
-                                baz()}.
+                                opaque_baz()}.
 
 -type foo() :: {'$#enum_mod03:foo.x'}.
 
