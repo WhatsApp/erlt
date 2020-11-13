6380426ed3a33a688d1f251a2c138050ecff2182
Per Gustafsson <pergu@fb.com>
2020-11-13 14:55:15 +0000

    Fix warnings for test cases in example.

--- examples/dev/ir-spec/lib.erl
+++ examples/dev/ir-spec/lib.erl
@@ -2,7 +2,7 @@
 
 -module(lib).
 
--export([id/1]).
+-export([id/1, test/0]).
 
 -export_type([public_enum/0, opaque_enum_alias/0]).
 
@@ -15,6 +15,14 @@
      semi_private_enum() :: {'$#lib:semi_private_enum.semi_private_ctr'}.
 
 -opaque opaque_enum_alias() :: semi_private_enum().
+
+use_private_enum({'$#lib:private_enum.private_ctr'}) ->
+    "good".
+
+-spec test() -> string().
+
+test() ->
+    use_private_enum({'$#lib:private_enum.private_ctr'}).
 
 -spec id(A) -> A.
 
