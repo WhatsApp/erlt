852d8f1ebeb21a8cebcfb9c83cd3db68383cd0a0
Tom Davies <tomdavies@fb.com>
2020-11-26 17:33:51 +0000

    Apply suggested changes from PR

--- analyzer/src/main/scala/com/whatsapp/analyzer/GenServerCalls.scala
+++ analyzer/src/main/scala/com/whatsapp/analyzer/GenServerCalls.scala
@@ -18,7 +18,7 @@ object GenServerCalls {
       totalCount = totalCount + total
       taggedCount = taggedCount + tagged
       othersCount = othersCount + others
-      if (others > 0 ) {
+      if (others > 0) {
         exceptions = Calls(module, total, tagged, others) :: exceptions
       }
     }
@@ -33,8 +33,8 @@ object GenServerCalls {
   }
 
   private def loadData(rpc: RPC): List[AppInfo] = {
-    val infos =CodeDirs.projectEbinDirs.map(indexProjectDir(_, rpc))
-    infos.filterNot {info => CodeDirs.thirdParty.contains(info.app)}
+    val infos = CodeDirs.projectEbinDirs.map(indexProjectDir(_, rpc))
+    infos.filterNot { info => CodeDirs.thirdParty.contains(info.app) }
   }
 
   private def indexProjectDir(dir: String, rpc: RPC): AppInfo = {
