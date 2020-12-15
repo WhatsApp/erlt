5a9fc5585c93b50579588f3d1b625089eda84841
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-12-15 18:00:13 +0000

    [equalizer] moduleApi: globalize

--- analyzer/src/main/scala/com/whatsapp/eqwalizer/ModuleApi.scala
+++ analyzer/src/main/scala/com/whatsapp/eqwalizer/ModuleApi.scala
@@ -6,8 +6,8 @@ import scala.collection.mutable.ListBuffer
 
 object ModuleApi {
   def apply(forms: List[AbstractForm]): ModuleApi = {
-    var module: AF_Module = null
-    var behaviour: Option[AF_Behaviour] = None
+    var module: String = null
+    var behaviours: ListBuffer[String] = ListBuffer()
     val exports: ListBuffer[IdWithArity] = ListBuffer()
     var imports: Map[IdWithArity, String] = Map.empty
     val exportTypes: ListBuffer[IdWithArity] = ListBuffer()
@@ -16,8 +16,8 @@ object ModuleApi {
     var types: ListBuffer[AF_TypeDecl] = ListBuffer()
     for (f <- forms)
       f match {
-        case m: AF_Module       => module = m
-        case b: AF_Behaviour    => behaviour = Some(b)
+        case m: AF_Module       => module = m.name
+        case b: AF_Behaviour    => behaviours += b.name
         case e: AF_Export       => exports ++= e.funs
         case i: AF_Import       => imports ++= i.funs.map(_ -> i.module)
         case e: AF_ExportType   => exportTypes ++= e.types
@@ -28,20 +28,20 @@ object ModuleApi {
       }
     ModuleApi(
       module,
-      behaviour,
+      behaviours.toList,
       exports.toList.sorted,
       imports,
       exportTypes.toList.sorted,
-      records.toList.sortBy(_.name),
-      specs.toList.sortBy(_.id),
-      types.toList.sortBy(t => (t.typeName, t.params.size)),
+      records.toList.sortBy(_.name).map(TypeUtils.globalizeRecord(module, _)),
+      specs.toList.sortBy(_.id).map(TypeUtils.globalizeSpec(module, _)),
+      types.toList.sortBy(t => (t.typeName, t.params.size)).map(TypeUtils.globalizeTypeDecl(module, _)),
     )
   }
 }
 
 case class ModuleApi(
-    module: AF_Module,
-    behaviour: Option[AF_Behaviour],
+    module: String,
+    behaviours: List[String],
     exports: List[IdWithArity],
     imports: Map[IdWithArity, String],
     exportTypes: List[IdWithArity],
---
+++ analyzer/src/main/scala/com/whatsapp/eqwalizer/TypeUtils.scala
@@ -0,0 +1,63 @@
+package com.whatsapp.eqwalizer
+
+import erlang.forms.AbstractForm._
+import erlang.forms.AbstractType._
+
+object TypeUtils {
+  // turns all "local" types into FQ remote types
+  def globalize(module: String, t: AbstractType): AbstractType =
+    t match {
+      case AF_AnnotatedType(anno, tp) =>
+        AF_AnnotatedType(anno, globalize(module, tp))
+      case AF_FunTypeAnyArgs(tp) =>
+        AF_FunTypeAnyArgs(globalize(module, tp))
+      case AF_FunctionType(args, resType) =>
+        AF_FunctionType(args.map(globalize(module, _)), globalize(module, resType))
+      case AF_PredefinedType(name, params) =>
+        AF_PredefinedType(name, params.map(globalize(module, _)))
+      case AF_AssocMap(assocs) =>
+        AF_AssocMap(assocs.map(globalizeAssoc(module, _)))
+      case AF_RecordType(recordName, fieldTypes) =>
+        AF_RecordType(recordName, fieldTypes.map(globalizeRecordField(module, _)))
+      case AF_RemoteType(module, name, params) =>
+        AF_RemoteType(module, name, params.map(globalize(module, _)))
+      case AF_TupleTypeTyped(params) =>
+        AF_TupleTypeTyped(params.map(globalize(module, _)))
+      case AF_TypeUnion(params) =>
+        AF_TypeUnion(params.map(globalize(module, _)))
+      case AF_UserDefinedType(typeName, types) =>
+        AF_RemoteType(module, typeName, types.map(globalize(module, _)))
+      /// CANNOT HAVE USER_TYPES INSIDE
+      case AF_TupleTypeAny | AF_AnyMap | AF_EmptyListType | AF_FunTypeAny | _: AF_IntegerRangeType | _: AF_AtomType |
+          _: AF_TypeVariable | _: AF_BitstringType | _: AF_SingletonIntegerType =>
+        t
+    }
+
+  private def globalizeAssoc(module: String, assoc: AF_AssocType): AF_AssocType =
+    assoc match {
+      case MapFieldExact(types) =>
+        MapFieldExact(types.map(globalize(module, _)))
+      case MapFieldAssoc(types) =>
+        MapFieldAssoc(types.map(globalize(module, _)))
+    }
+
+  private def globalizeRecordField(module: String, field: AF_RecordFieldType): AF_RecordFieldType =
+    field.copy(tp = globalize(module, field.tp))
+
+  def globalizeSpec(module: String, spec: AF_FunctionSpec): AF_FunctionSpec = {
+    val types = spec.types.map {
+      case AF_ConstrainedFunctionType(AF_FunctionType(args, res), constraints) =>
+        AF_ConstrainedFunctionType(
+          AF_FunctionType(args.map(globalize(module, _)), globalize(module, res)),
+          constraints.map { case Constraint(v, tp) => Constraint(v, globalize(module, tp)) },
+        )
+    }
+    spec.copy(types = types)
+  }
+
+  def globalizeRecord(module: String, rec: AF_RecordDecl): AF_RecordDecl =
+    rec.copy(fields = rec.fields.map(f => f.copy(tp = f.tp.map(globalize(module, _)))))
+
+  def globalizeTypeDecl(module: String, decl: AF_TypeDecl): AF_TypeDecl =
+    decl.copy(abstractType = globalize(module, decl.abstractType))
+}
