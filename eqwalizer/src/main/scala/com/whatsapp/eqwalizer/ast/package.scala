350d60c75c27556d2690dfca7940d7c558d8922e
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-01-11 08:47:39 +0000

    [eqwalizer] concepts of App and ModuleStub

--- eqwalizer/src/main/scala/com/whatsapp/eqwalizer/ast/package.scala
+++ eqwalizer/src/main/scala/com/whatsapp/eqwalizer/ast/package.scala
@@ -16,6 +16,8 @@
 
 package com.whatsapp.eqwalizer
 
+import com.whatsapp.eqwalizer.ast.Forms._
+
 package object ast {
   case class Id(name: String, arity: Int) {
     override def toString: String = s"$name/$arity"
@@ -23,4 +25,21 @@ package object ast {
   case class RemoteId(module: String, name: String, arity: Int) {
     override def toString: String = s"$module:$name/$arity"
   }
+
+  case class App(
+      name: String,
+      ebinDir: String,
+      modules: List[String],
+  )
+
+  case class ModuleStub(
+      module: String,
+      exports: Set[Id],
+      imports: Map[Id, String],
+      exportTypes: Set[Id],
+      specs: Map[Id, FunSpec],
+      types: Map[Id, TypeDecl],
+      skippedSpecs: Map[Id, SkippedFunSpec],
+      skippedTypes: Map[Id, SkippedTypeDecl],
+  )
 }
