fb151a0cd94360757e860486a4bfd1acaed64069
Alan Zimmerman <alanzimm@fb.com>
2021-01-29 17:42:26 +0000

    [h-elp] first pass integration of TS into DidOpen event
    Parses and logs the initial full parse.

--- elp/h-elp/exe/Main.hs
+++ elp/h-elp/exe/Main.hs
@@ -1,47 +1,318 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
+{-# LANGUAGE TypeInType #-}
 
--- First cut, based on Simple.hs example in the lsp package
-
+import Control.Lens hiding (Iso, List)
+import Control.Monad
+import Control.Monad.Reader
+import Data.Aeson hiding (Options, defaultOptions)
+import qualified Data.HashMap.Strict as HM
+import Data.List (isSuffixOf)
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import ELP
+import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array (mallocArray)
+import Foreign.Ptr
+import Foreign.Storable
+import GHC.Generics (Generic)
+import GHC.Word
+import Language.LSP.Diagnostics (partitionBySource)
 import Language.LSP.Server
 import Language.LSP.Types
-import Control.Monad.IO.Class
-import qualified Data.Text as T
+import Language.LSP.Types.Lens as Lsp hiding (options, publishDiagnostics, textDocumentSync)
+import Language.LSP.VFS
+import System.Directory
+import System.FilePath
+import System.Log.Logger
+import TreeSitter.ErlangELP
+import TreeSitter.Node
+import TreeSitter.Parser
+import TreeSitter.Tree
+import UnliftIO
+import UnliftIO.Concurrent
 
-handlers :: Handlers (LspM ())
-handlers = mconcat
-  [ notificationHandler SInitialized $ \_not -> do
-      let params = ShowMessageRequestParams MtInfo "Turn on code lenses?"
-            (Just [MessageActionItem "Turn on", MessageActionItem "Don't"])
-      _ <- sendRequest SWindowShowMessageRequest params $ \res ->
-        case res of
-          Right (Just (MessageActionItem "Turn on")) -> do
-            let regOpts = CodeLensRegistrationOptions Nothing Nothing (Just False)
-
-            _ <- registerCapability STextDocumentCodeLens regOpts $ \_req responder -> do
-              let cmd = Command "Say hello" "lsp-hello-command" Nothing
-                  rsp = List [CodeLens (mkRange 0 0 0 100) (Just cmd) Nothing]
-              responder (Right rsp)
-            pure ()
-          Right _ ->
-            sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Not turning on code lenses")
-          Left err ->
-            sendNotification SWindowShowMessage (ShowMessageParams MtError $ "Something went wrong!\n" <> T.pack (show err))
-      pure ()
-  , requestHandler STextDocumentHover $ \req responder -> do
-      let RequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
-          Position _l _c' = pos
-          rsp = Hover ms (Just range)
-          ms = HoverContents $ markedUpContent "lsp-demo-simple-server" "Hello world"
-          range = Range pos pos
-      responder (Right $ Just rsp)
-  ]
+-- ---------------------------------------------------------------------
 
 main :: IO Int
-main = runServer $ ServerDefinition
-  { onConfigurationChange = const $ pure $ Right ()
-  , doInitialize = \env _req -> pure $ Right env
-  , staticHandlers = handlers
-  , interpretHandler = \env -> Iso (runLspT env) liftIO
-  , options = defaultOptions
+main = do
+  -- Log to stderr by default
+  -- setupLogger Nothing ["h-elp"] DEBUG
+  setupLogger Nothing ["h-elp"] INFO
+
+  parser <- treeSitterParser
+  let pc = ParserContext parser mempty
+  handlerEnv <- HandlerEnv <$> newEmptyMVar <*> newEmptyMVar <*> newMVar pc
+
+  runServer $
+    ServerDefinition
+      { doInitialize = \env _req -> pure $ Right env,
+        onConfigurationChange = const $ pure $ Right defaultConfig,
+        staticHandlers = handlers,
+        interpretHandler = \env ->
+          Iso
+            (\m -> runLspT env (runReaderT m handlerEnv))
+            liftIO,
+        options = lspOptions
+      }
+
+-- ---------------------------------------------------------------------
+
+lspOptions :: Options
+lspOptions =
+  defaultOptions
+    { textDocumentSync = Just syncOptions
+    }
+  where
+    syncOptions =
+      TextDocumentSyncOptions openClose change willSave willSaveWaitUntil save
+    openClose = Just True
+    change = Just TdSyncIncremental
+    willSave = Just False
+    willSaveWaitUntil = Just False
+    save = Just $ InR $ SaveOptions $ Just False
+
+-- ---------------------------------------------------------------------
+
+data HandlerEnv = HandlerEnv
+  { relRegToken :: MVar (RegistrationToken WorkspaceDidChangeWatchedFiles),
+    absRegToken :: MVar (RegistrationToken WorkspaceDidChangeWatchedFiles),
+    parser :: MVar ParserContext
   }
+
+data ParserContext = ParserContext
+  { pcParser :: !(Ptr Parser),
+    pcsMap :: Map.Map NormalizedUri TsFile
+  }
+
+data TsFile = TsFile
+  { pcTree :: !(Ptr Tree),
+    pcNode :: !Node
+  }
+
+-- ---------------------------------------------------------------------
+
+handlers :: Handlers (ReaderT HandlerEnv (LspM Config))
+handlers =
+  mconcat
+    [ notificationHandler SInitialized $
+        \_noti -> do
+          sendNotification SWindowLogMessage $
+            LogMessageParams MtLog "initialized"
+
+          sendNotification
+            SWindowShowMessage
+            (ShowMessageParams MtInfo "Not turning on code lenses"),
+      -- -----------------------------------
+
+      notificationHandler STextDocumentDidOpen $ \msg -> do
+        let doc = msg ^. params . textDocument . uri
+            fileName = uriToFilePath doc
+            docUri = toNormalizedUri doc
+        let aContent = msg ^. params . textDocument . text
+        liftIO $ infoM "h-elp" $ "Processing DidOpenTextDocument for: " ++ show fileName
+        sendDiagnostics docUri (Just 0)
+        mdoc <- getVirtualFile docUri
+        case mdoc of
+          Just (VirtualFile _lspversion version rope) -> do
+            liftIO $ infoM "h-elp" $ "Found the virtual file: " ++ show version
+            mparser <- asks parser
+            -- modifyMVar :: MVar a -> (a -> IO (a, b)) -> IO b
+            liftIO $
+              modifyMVar mparser $ \(ParserContext parser pcsmap) -> do
+                (tree, node) <- treeSitterParseFull parser (T.unpack aContent)
+                sexp <- nodeAsSexpr node
+                liftIO $ infoM "h-elp" $ "parsed to: " ++ show sexp
+                let pcsmap' = Map.insert docUri (TsFile tree node) pcsmap
+                return (ParserContext parser pcsmap', ())
+            return ()
+          Nothing -> do
+            liftIO $ infoM "h-elp" $ "Didn't find anything in the VFS for: " ++ show doc
+        return (),
+      -- -----------------------------------
+
+      notificationHandler STextDocumentDidChange $ \msg -> do
+        let doc = msg ^. params . textDocument . uri . to toNormalizedUri
+        liftIO $ infoM "h-elp" $ "Processing DidChangeTextDocument for: " ++ show doc
+        mdoc <- getVirtualFile doc
+        case mdoc of
+          Just (VirtualFile _lspversion version rope) -> do
+            liftIO $ infoM "h-elp" $ "Found the virtual file: " ++ show version
+          Nothing -> do
+            liftIO $ infoM "h-elp" $ "Didn't find anything in the VFS for: " ++ show doc,
+      -- ===================================
+      -- -----------------------------------
+
+      requestHandler STextDocumentHover $
+        \_req responder ->
+          responder $
+            Right $
+              Just $
+                Hover (HoverContents (MarkupContent MkPlainText "hello")) Nothing,
+      -- -----------------------------------
+
+      requestHandler STextDocumentDocumentSymbol $
+        \_req responder ->
+          responder $
+            Right $
+              InL $
+                List
+                  [ DocumentSymbol
+                      "foo"
+                      Nothing
+                      SkObject
+                      Nothing
+                      (mkRange 0 0 3 6)
+                      (mkRange 0 0 3 6)
+                      Nothing
+                  ],
+      -- -----------------------------------
+
+      -- notificationHandler STextDocumentDidOpen $
+      --   \noti -> do
+      --     let NotificationMessage _ _ (DidOpenTextDocumentParams doc) = noti
+      --         TextDocumentItem uri _ _ _ = doc
+      --         Just fp = uriToFilePath uri
+      --         diag =
+      --           Diagnostic
+      --             (mkRange 0 0 0 1)
+      --             (Just DsWarning)
+      --             (Just (InL 42))
+      --             (Just "dummy-server")
+      --             "Here's a warning"
+      --             Nothing
+      --             Nothing
+      --     withRunInIO $
+      --       \runInIO -> do
+      --         when (".hs" `isSuffixOf` fp) $
+      --           void $
+      --             forkIO $
+      --               do
+      --                 threadDelay (2 * 10 ^ 6)
+      --                 runInIO $
+      --                   sendNotification STextDocumentPublishDiagnostics $
+      --                     PublishDiagnosticsParams uri Nothing (List [diag])
+      --         -- also act as a registerer for workspace/didChangeWatchedFiles
+      --         when (".register" `isSuffixOf` fp) $
+      --           do
+      --             let regOpts =
+      --                   DidChangeWatchedFilesRegistrationOptions $
+      --                     List
+      --                       [ FileSystemWatcher
+      --                           "*.watch"
+      --                           (Just (WatchKind True True True))
+      --                       ]
+      --             Just token <- runInIO $
+      --               registerCapability SWorkspaceDidChangeWatchedFiles regOpts $
+      --                 \_noti ->
+      --                   sendNotification SWindowLogMessage $
+      --                     LogMessageParams MtLog "got workspace/didChangeWatchedFiles"
+      --             runInIO $ asks relRegToken >>= \v -> putMVar v token
+      --         when (".register.abs" `isSuffixOf` fp) $
+      --           do
+      --             curDir <- getCurrentDirectory
+      --             let regOpts =
+      --                   DidChangeWatchedFilesRegistrationOptions $
+      --                     List
+      --                       [ FileSystemWatcher
+      --                           (curDir </> "*.watch")
+      --                           (Just (WatchKind True True True))
+      --                       ]
+      --             Just token <- runInIO $
+      --               registerCapability SWorkspaceDidChangeWatchedFiles regOpts $
+      --                 \_noti ->
+      --                   sendNotification SWindowLogMessage $
+      --                     LogMessageParams MtLog "got workspace/didChangeWatchedFiles"
+      --             runInIO $ asks absRegToken >>= \v -> putMVar v token
+      --         -- also act as an unregisterer for workspace/didChangeWatchedFiles
+      --         when (".unregister" `isSuffixOf` fp) $
+      --           do
+      --             Just token <- runInIO $ asks relRegToken >>= tryReadMVar
+      --             runInIO $ unregisterCapability token
+      --         when (".unregister.abs" `isSuffixOf` fp) $
+      --           do
+      --             Just token <- runInIO $ asks absRegToken >>= tryReadMVar
+      --             runInIO $ unregisterCapability token,
+
+      -- -----------------------------------
+
+      -- requestHandler SWorkspaceExecuteCommand $ \req resp -> do
+      --   let RequestMessage _ _ _ (ExecuteCommandParams Nothing "doAnEdit" (Just (List [val]))) = req
+      --       Success docUri = fromJSON val
+      --       edit = List [TextEdit (mkRange 0 0 0 5) "howdy"]
+      --       params =
+      --         ApplyWorkspaceEditParams (Just "Howdy edit") $
+      --           WorkspaceEdit (Just (HM.singleton docUri edit)) Nothing
+      --   resp $ Right Null
+      --   void $ sendRequest SWorkspaceApplyEdit params (const (pure ())),
+      -- -----------------------------------
+
+      requestHandler STextDocumentCodeAction $ \req resp -> do
+        let RequestMessage _ _ _ params = req
+            CodeActionParams _ _ _ _ cactx = params
+            CodeActionContext diags _ = cactx
+            codeActions = fmap diag2ca diags
+            diag2ca d =
+              CodeAction
+                "Delete this"
+                Nothing
+                (Just (List [d]))
+                Nothing
+                Nothing
+                (Just (Command "" "deleteThis" Nothing))
+        resp $ Right $ InR <$> codeActions,
+      -- -----------------------------------
+
+      requestHandler STextDocumentCompletion $ \_req resp -> do
+        let res = CompletionList True (List [item])
+            item =
+              CompletionItem
+                "foo"
+                (Just CiConstant)
+                (Just (List []))
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+        resp $ Right $ InR res
+    ]
+
+-- ---------------------------------------------------------------------
+
+-- | Analyze the file and send any diagnostics to the client in a
+-- "textDocument/publishDiagnostics" notification
+-- sendDiagnostics :: NormalizedUri -> Maybe Int -> LspM Config ()
+sendDiagnostics :: MonadLsp config m => NormalizedUri -> TextDocumentVersion -> m ()
+sendDiagnostics fileUri version = do
+  let diags =
+        [ Diagnostic
+            (Range (Position 0 1) (Position 0 5))
+            (Just DsWarning) -- severity
+            Nothing -- code
+            (Just "lsp-hello") -- source
+            "Example diagnostic message"
+            Nothing -- tags
+            (Just (List []))
+        ]
+  publishDiagnostics 100 fileUri version (partitionBySource diags)
+
+-- ---------------------------------------------------------------------
+
+data Config = Config {fooTheBar :: Bool, wibbleFactor :: Int}
+  deriving (Generic, ToJSON, FromJSON)
+
+defaultConfig :: Config
+defaultConfig = Config True 0
+
+-- ---------------------------------------------------------------------
--- elp/h-elp/src/ELP.hs
+++ elp/h-elp/src/ELP.hs
@@ -1,4 +1,198 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NumericUnderscores #-}
+
 module ELP where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad
+import           Data.Hashable
+import qualified Data.Map as Map
+import           Data.Rope.UTF16 ( Rope )
+import qualified Data.Rope.UTF16 as Rope
+import qualified Data.Text as T
+import           GHC.Word
+import           Foreign.C.String
+import           Foreign.Marshal.Alloc
+import           Foreign.Marshal.Array          ( mallocArray )
+import           Foreign.Ptr
+import           Foreign.Storable
+import           Language.Haskell.LSP.Test
+import           Language.LSP.Types
+import           Language.LSP.Types.Lens
+import           Language.LSP.VFS
+import           System.Directory
+import           System.FilePath
+import           System.IO
+import           System.IO.Temp
+import           System.Log.Logger
+import           TreeSitter.ErlangELP
+import           TreeSitter.Node
+import           TreeSitter.Parser
+import           TreeSitter.Tree
+
+
+-- ---------------------------------------------------------------------
+
+-- | Print st stderr, where the haskell tree-sitter logging goes
+-- too. Convenient, it does not get in the way of tasty output either.
+pp :: String -> IO ()
+pp = hPutStrLn stderr
+
+-- TODO: free the parser, or bracket it with a free
+treeSitterParser :: IO (Ptr Parser)
+treeSitterParser = do
+  parser <- ts_parser_new
+  pp $ "got parser:" ++ show parser
+  timeout <- ts_parser_timeout_micros parser
+  pp $ "timeout:" ++ show timeout
+
+  ts_parser_set_timeout_micros parser 3_000_0000
+
+  timeout <- ts_parser_timeout_micros parser
+  pp $ "timeout:" ++ show timeout
+
+  r <- ts_parser_set_language parser tree_sitter_erlang_elp
+  pp $ "set language:" ++ show (r, tree_sitter_erlang_elp)
+  return parser
+
+treeSitterParseFull :: Ptr Parser -> String -> IO (Ptr Tree, Node)
+treeSitterParseFull parser source = do
+  (str, len) <- newCStringLen source
+  tree       <- ts_parser_parse_string parser nullPtr str len
+  -- pp $ "got tree:" ++ show tree
+  n          <- malloc
+  ts_tree_root_node_p tree n
+  -- pp $ "after ts_tree_root_node_p"
+  nn <- peek n
+  -- pp $ "Node:" ++ show nn
+  return (tree, nn)
+
+treeSitterParseEdit :: TSInputEdit -> Ptr Tree -> IO (Ptr Tree)
+treeSitterParseEdit edit tree = do
+  inp <- malloc
+  poke inp edit
+  ts_tree_edit tree inp
+  return tree
+
+-- Based on Language.LSP.VFS.applyChange.
+-- TODO: move this somewhere meaningful
+lspChangeAsTSInputEdit :: Rope -> TextDocumentContentChangeEvent -> TSInputEdit
+lspChangeAsTSInputEdit rope change = edit
+  where
+    edit = case change of
+      (TextDocumentContentChangeEvent Nothing Nothing txt) ->
+        -- replace the entire string
+        let
+        in TSInputEdit
+             { editStartByte   = 0
+             , editOldEndByte  = fromIntegral $ Rope.length rope
+             , editNewEndByte  = fromIntegral $ T.length txt
+             , editStartPoint  = TSPoint 0 0
+             , editOldEndPoint = TSPoint (fromIntegral $ Rope.rows rope) 0
+               -- probably have to use something other than hard coded
+               -- col of zero. First approximation.
+             , editNewEndPoint = TSPoint (fromIntegral $ Rope.rows (Rope.fromText txt)) 0
+               -- If we integrate this with the VFS change processing,
+               -- the Rope.fromText will not be wasted.
+             }
+      (TextDocumentContentChangeEvent (Just (Range from to)) _ txt) ->
+        -- replace a portion with a new string.  The length parameter
+        -- is optional and deprecated
+        let
+          startByte = byteAddress rope from
+          endByte   = byteAddress rope to
+          startPoint = position2TSPoint from
+        in TSInputEdit
+             { editStartByte   = startByte
+             , editOldEndByte  = endByte
+             , editNewEndByte  = startByte + fromIntegral (T.length txt)
+             , editStartPoint  = startPoint
+             , editOldEndPoint = position2TSPoint to
+               -- probably have to use something other than hard coded
+               -- col of zero. First approximation.
+             , editNewEndPoint = addText startPoint txt
+               -- If we integrate this with the VFS change processing,
+               -- the Rope.fromText will not be wasted.
+             }
+      (TextDocumentContentChangeEvent Nothing (Just _) _txt) -> undefined
+
+    byteAddress :: Rope -> Position -> Word32
+    byteAddress rope (Position line col)
+      = fromIntegral $ Rope.rowColumnCodeUnits (Rope.RowColumn line col) rope
+
+    position2TSPoint :: Position -> TSPoint
+    position2TSPoint (Position line col) = TSPoint (fromIntegral line) (fromIntegral col)
+
+    addText :: TSPoint -> T.Text -> TSPoint
+    addText (TSPoint row col) txt = TSPoint row' col'
+      where
+        rope = Rope.fromText txt
+        newRows = Rope.rows rope
+        lastCol = Rope.length $ snd $ Rope.splitAtLine newRows rope
+        (row', col') = case newRows of
+          0 -> ( row
+               , col + fromIntegral lastCol)
+          r -> ( row + fromIntegral r
+               , fromIntegral lastCol)
+
+
+treeSitterParseIncrement :: Ptr Parser -> Rope -> Ptr Tree -> IO (Ptr Tree, Node)
+treeSitterParseIncrement parser rope tree = do
+
+  -- let source2 =
+  --       -- "-moable(foo).\n-x(y).\n"
+  --       "-abcdef(foo).\n-xxxx(yyy).\n"
+  let source2 = Rope.toString rope
+  (str2, _len2) <- newCStringLen source2
+
+  fp <- mkReadFunction vfsReadFunction
+  let i = TSHInput
+            { inputPayload = castPtr str2
+            , inputRead = fp
+            , inputEncoding = TSInputEncodingUTF8
+            }
+  pp $ "input:" ++ show i
+  ip <- malloc
+  poke ip i
+  ts_parser_log_to_stderr parser
+  -- tree2       <- ts_parser_parse_string parser tree str2 len2
+  pp $ "invoking ts_parser_parse_p1"
+  tree       <- ts_parser_parse_p1 parser tree ip
+  pp $ "got tree:" ++ show tree
+  n          <- malloc
+  ts_tree_root_node_p tree n
+  -- pp $ "after ts_tree_root_node_p"
+  nn <- peek n
+  -- pp $ "Node:" ++ show nn
+  return (tree, nn)
+
+
+-- | Tree Sitter 'ReadFunction' using a VFS as the payload
+vfsReadFunction :: ReadFunction
+vfsReadFunction ppayload offset ppos lenPtr = do
+  pp $ "******vfsReadFunction entered"
+  pp $ "******vfsReadFunction ppayload=" ++ show (ppayload, offset, ppos)
+  -- type ReadFunction = (Ptr Payload -> Word32 -> Ptr TSPoint -> Ptr Word32 -> IO CString)
+  tin <- peek ppayload
+  let pstr = plusPtr (inputPayload tin) (fromIntegral offset)
+  str <- peekCString pstr
+  hPutStrLn stderr $ "******vfsReadFunction str=[" ++ show str ++ "]"
+  len <- withCStringLen str $ \(_s,l) -> return l
+  poke lenPtr (fromIntegral len)
+  -- pp $ "******vfsReadFunction ending=" ++ show (pstr, len)
+  return pstr
+
+nodeAsSexpr :: Node -> IO String
+nodeAsSexpr nn = do
+  let tsn = nodeTSNode nn
+  pp $ "tsn:" ++ show tsn
+  tsnp <- malloc
+  poke tsnp tsn
+  pp $ "after poke"
+  strp <- ts_node_string_p tsnp
+  pp $ "after poke 2"
+  str <- peekCString strp
+  free strp
+  pp $ "after poke"
+  pp $ "node sexpr:" ++ str
+  return str
