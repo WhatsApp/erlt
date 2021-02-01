bdd33a310d745cdf7109f9a5fcbb199df4ee68c4
Michał Muskała <micmus@whatsapp.com>
2021-02-01 11:14:23 +0000

    [elp] Use fields & supertypes in tree-sitter

--- elp/baltazar/crates/tree-sitter-erlang/src/lib.rs
+++ elp/baltazar/crates/tree-sitter-erlang/src/lib.rs
@@ -1,9 +1,9 @@
 use tree_sitter::Language;
 
 extern "C" {
-    fn tree_sitter_erlang() -> Language;
+    fn tree_sitter_erlang_elp() -> Language;
 }
 
 pub fn language() -> Language {
-    unsafe { tree_sitter_erlang() }
+    unsafe { tree_sitter_erlang_elp() }
 }
