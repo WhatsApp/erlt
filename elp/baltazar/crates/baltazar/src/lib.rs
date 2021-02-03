908822c87886a03fa32efc5fc032515eb3a5f61b
Michał Muskała <micmus@whatsapp.com>
2021-02-03 16:10:11 +0000

    [baltazar] Simple LSP & basic client for debugging

--- elp/baltazar/crates/baltazar/src/lib.rs
+++ elp/baltazar/crates/baltazar/src/lib.rs
@@ -1,1 +1,10 @@
 pub use anyhow::Result;
+
+use anyhow::anyhow;
+use serde::de::DeserializeOwned;
+
+pub fn from_json<T: DeserializeOwned>(what: &'static str, json: serde_json::Value) -> Result<T> {
+    let res = serde_path_to_error::deserialize(&json)
+        .map_err(|e| anyhow!("Failed to deserialize {}: {}; {}", what, e, json))?;
+    Ok(res)
+}
