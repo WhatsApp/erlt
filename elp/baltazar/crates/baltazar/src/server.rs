3d54de65eec4bd21fcc7f9aa204976f05cdc9d79
Michał Muskała <micmus@whatsapp.com>
2021-02-08 09:40:22 +0100

    [baltazar] Basic request handling

--- elp/baltazar/crates/baltazar/src/server.rs
+++ elp/baltazar/crates/baltazar/src/server.rs
@@ -1,18 +1,14 @@
-use std::{fmt, sync::Arc};
+use std::{fmt, sync::Arc, time::Instant};
 
 use anyhow::{bail, Result};
 use crossbeam_channel::{select, Receiver, Sender};
-use lsp_server::Message;
-use lsp_types::notification::{self, Notification};
+use lsp_server::{ErrorCode, Message, RequestId, Response};
+use lsp_types::{
+    notification::{self, Notification},
+    request::{self, Request},
+};
 
 use crate::config::Config;
-
-pub struct Server {
-    sender: Sender<Message>,
-    lsp_receiver: Receiver<Message>,
-    config: Arc<Config>,
-}
-
 enum Event {
     Lsp(Message),
 }
@@ -31,9 +27,33 @@ impl fmt::Debug for Event {
     }
 }
 
+type ReqHandler = fn(&mut Server, Response);
+type ReqQueue = lsp_server::ReqQueue<(String, Instant), ReqHandler>;
+
+#[derive(Debug, Clone, Copy)]
+enum Status {
+    Loading,
+    Running,
+    ShuttingDown,
+}
+
+pub struct Server {
+    sender: Sender<Message>,
+    lsp_receiver: Receiver<Message>,
+    req_queue: ReqQueue,
+    config: Arc<Config>,
+    status: Status,
+}
+
 impl Server {
     pub fn new(sender: Sender<Message>, receiver: Receiver<Message>, config: Config) -> Server {
-        Server { sender, lsp_receiver: receiver, config: Arc::new(config) }
+        Server {
+            sender,
+            lsp_receiver: receiver,
+            req_queue: ReqQueue::default(),
+            config: Arc::new(config),
+            status: Status::Loading,
+        }
     }
 
     pub fn main_loop(mut self) -> Result<()> {
@@ -58,7 +78,63 @@ impl Server {
     }
 
     fn handle_event(&mut self, event: Event) -> Result<()> {
+        let loop_start = Instant::now();
+
         log::info!("handle_event {:?}", event);
+
+        match event {
+            Event::Lsp(msg) => match msg {
+                Message::Request(req) => self.on_request(loop_start, req)?,
+                _ => (),
+                // Message::Notification(notif) => self.on_notification(notif)?,
+                // Message::Response(resp) => self.complete_request(resp)?,
+            },
+        }
+
         Ok(())
+    }
+
+    fn on_request(&mut self, received: Instant, req: lsp_server::Request) -> Result<()> {
+        self.register_request(&req, received);
+
+        match self.status {
+            Status::Loading if req.method != request::Shutdown::METHOD => {
+                self.respond(Response::new_err(
+                    req.id,
+                    ErrorCode::ContentModified as i32,
+                    "baltazar is still loading".to_string(),
+                ))
+            }
+            Status::ShuttingDown => self.respond(Response::new_err(
+                req.id,
+                ErrorCode::InvalidRequest as i32,
+                "shutdown already requested".to_string(),
+            )),
+            _ => {}
+        }
+
+        Ok(())
+    }
+
+    fn respond(&mut self, response: Response) {
+        if let Some((method, start)) = self.req_queue.incoming.complete(response.id.clone()) {
+            let duration = start.elapsed();
+            log::info!("handled req#{} in {:?}", response.id, duration);
+            self.send(response.into());
+        }
+    }
+
+    fn cancel(&mut self, request_id: RequestId) {
+        if let Some(response) = self.req_queue.incoming.cancel(request_id) {
+            self.send(response.into());
+        }
+    }
+
+    fn send(&mut self, message: Message) {
+        self.sender.send(message).unwrap()
+    }
+
+    fn register_request(&mut self, request: &lsp_server::Request, received: Instant) {
+        self.req_queue.incoming.register(request.id.clone(), (request.method.clone(), received))
     }
 }
