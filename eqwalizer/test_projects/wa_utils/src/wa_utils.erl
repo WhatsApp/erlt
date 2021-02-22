02990daf1ab58932b8f12c3c2bd1d4d306a2e959
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-02-22 18:00:35 +0000

    not building core for now

--- eqwalizer/test_projects/wa_utils/src/wa_utils.erl
+++ eqwalizer/test_projects/wa_utils/src/wa_utils.erl
@@ -3,7 +3,7 @@
 -export([init/1]).
 
 init(State0) ->
-    Providers = [wa_build_info_prv, wa_corec],
+    Providers = [wa_build_info_prv],
     State1 = lists:foldl(
         fun(Provider, State) ->
             {ok, NewState} = Provider:init(State),
